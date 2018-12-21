6fba1430767e8771049d9c79bea826847d5a5c4f
Raymond Chenon <raymond.chenon@gmail.com>
2018-12-20 14:36:48 -1000

    Tests for condition camera can only be installed on top of an existing parking space.

--- src/test/java/com/raychenon/ParkingCamerasTest.java
+++ src/test/java/com/raychenon/ParkingCamerasTest.java
@@ -7,11 +7,15 @@ import static junit.framework.TestCase.assertEquals;
 public class ParkingCamerasTest {
     
     @Test
-    public void assertSolution() {
+    public void assertParkingCameras() {
         assertEquals(ParkingCamerasSolution.findMinimumNumberOfCameras(1, new int[] { 1, 2, 3, 4, 5}), 2);
         assertEquals(ParkingCamerasSolution.findMinimumNumberOfCameras(2, new int[] { 2, 4, 5, 6, 7, 9, 11, 12}), 3);
         assertEquals(ParkingCamerasSolution.findMinimumNumberOfCameras(10, new int[] { 1, 15, 30, 40, 50}), 3);
         assertEquals(ParkingCamerasSolution.findMinimumNumberOfCameras(10, new int[] { 1, 15, 30,31,32,33,39, 40, 50}), 3);
+
+        //  REMEMBER that camera can only be installed on top of an existing parking space.
+        assertEquals(ParkingCamerasSolution.findMinimumNumberOfCameras(10, new int[] { 10, 19, 30}), 2);
+        assertEquals(ParkingCamerasSolution.findMinimumNumberOfCameras(10, new int[] { 10, 20, 30}), 1);
     }
 
 }
