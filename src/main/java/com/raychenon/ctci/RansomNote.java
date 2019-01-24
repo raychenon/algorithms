5dceac1fa9690365eb34bc7b2ed0ea1335e798c1
Raymond Chenon <raymond.chenon@gmail.com>
2019-01-24 12:19:39 +0100

    #10 Ransom Note signature in Java

---
+++ src/main/java/com/raychenon/ctci/RansomNote.java
@@ -0,0 +1,14 @@
+package com.raychenon.ctci;
+
+/**
+ * User: raychenon
+ * Date: 24/1/19
+ * https://www.hackerrank.com/challenges/ctci-ransom-note/problem
+ */
+public class RansomNote {
+
+    static String checkMagazine(String[] magazine, String[] note) {
+
+      return "No";
+    }
+}
