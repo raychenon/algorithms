b03c1e24ad4ac0f2fceddb8f5ca5226b0c9ffcb3
Raymond Chenon <raymond.chenon@gmail.com>
2019-01-27 23:07:43 +0100

    #17 try to construct a DoublyLinkedListNode

--- src/test/java/com/raychenon/hackerrank/ReverseDoublyLinkedListNodeTest.java
+++ src/test/java/com/raychenon/hackerrank/ReverseDoublyLinkedListNodeTest.java
@@ -2,7 +2,7 @@ package com.raychenon.hackerrank;
 
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
@@ -13,34 +13,60 @@ import static junit.framework.TestCase.assertEquals;
  */
 public class ReverseDoublyLinkedListNodeTest {
 
+    public static void main(String [ ] args) {
+        
+        readDoublyLinkedList(createLinkedList(1,2,3,4));
+    }
+
     @Test
     public void assertReverseDoublyLinkedListNode() {
-        assertEquals(ReverseDoublyLinkedListNode.reverse(
-                createLinkedList(Arrays.asList(1,2,3,4))),
-                createLinkedList(Arrays.asList(4,3,2,1))
+
+        ReverseDoublyLinkedListNode.DoublyLinkedListNode node = createLinkedList(1,2,3,4);
+        ReverseDoublyLinkedListNode.DoublyLinkedListNode nodeReversed = createLinkedList(4,3,2,1);
+        readDoublyLinkedList(node);
+        readDoublyLinkedList(nodeReversed);
+        
+        assertEquals(ReverseDoublyLinkedListNode.reverse(node),
+                nodeReversed
         );
+    }
+
+    static  ReverseDoublyLinkedListNode.DoublyLinkedListNode createLinkedList(int ... args){
+        List<Integer> list = new LinkedList<Integer>();
+        for(int i : args){
+            list.add(i);
+        }
+        return createLinkedList(list);
     }
     
     static ReverseDoublyLinkedListNode.DoublyLinkedListNode createLinkedList(List<Integer> data) {
-        if(data.size() > 0){
+        if(data.size() == 0){
             return null;
         }
 
         ReverseDoublyLinkedListNode.DoublyLinkedListNode head = new ReverseDoublyLinkedListNode.DoublyLinkedListNode();
-        ReverseDoublyLinkedListNode.DoublyLinkedListNode current = new ReverseDoublyLinkedListNode.DoublyLinkedListNode();
-        head.data = data.get(0);
-        head.next = current.prev;
-        head.prev = null;
+        ReverseDoublyLinkedListNode.DoublyLinkedListNode temp = new ReverseDoublyLinkedListNode.DoublyLinkedListNode();
 
-        ReverseDoublyLinkedListNode.DoublyLinkedListNode next = new ReverseDoublyLinkedListNode.DoublyLinkedListNode();
-
-        for(Integer item: data.subList(1,data.size()-1)){
-            current.data = item;
-            
-            current.next = next.prev;
+        for(int i=0;i<data.size();i++){
+            ReverseDoublyLinkedListNode.DoublyLinkedListNode current = new ReverseDoublyLinkedListNode.DoublyLinkedListNode();
+            current.data = data.get(i);
+            current.prev = temp;
+            temp.next = current;
+            if (i == 0){
+                head = current;
+            }
+            temp = current;
         }
-        next.next = null;
 
         return head;
+    }
+
+    static void readDoublyLinkedList(ReverseDoublyLinkedListNode.DoublyLinkedListNode node){
+        ReverseDoublyLinkedListNode.DoublyLinkedListNode temp = node;
+
+        while(temp != null){
+            System.out.println("Data  " + temp.data + ", next = " + temp.next + ", prev = " + temp.prev);
+            temp = temp.next;
+        }
     }
 }
