b523bdd16b0e20fc247f2f9444aba1cae15b6141
Raymond Chenon <raymond.chenon@gmail.com>
2019-01-27 14:50:37 +0100

    #17 init

---
+++ src/main/java/com/raychenon/hackerrank/ReverseDoublyLinkedListNode.java
@@ -0,0 +1,21 @@
+package com.raychenon.hackerrank;
+
+/**
+ * User: raychenon
+ * Date: 27/1/19
+ */
+public class ReverseDoublyLinkedListNode {
+
+    static class DoublyLinkedListNode {
+        int data;
+        DoublyLinkedListNode next;
+        DoublyLinkedListNode prev;
+    }
+
+    static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+
+        // tail
+        return head;
+    }
+
+}
