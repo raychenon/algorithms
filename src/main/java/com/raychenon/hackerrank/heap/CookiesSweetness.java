b7dcdd7945b91c10959a3523a2084da7eac3f37c
Raymond Chenon <raymond.chenon@gmail.com>
2019-02-01 19:58:45 +0100

    #20 heap problem, cookie sweetness

---
+++ src/main/java/com/raychenon/hackerrank/heap/CookiesSweetness.java
@@ -0,0 +1,14 @@
+package com.raychenon.hackerrank.heap;
+
+/**
+ * User: raychenon
+ * Date: 1/2/19
+ * https://www.hackerrank.com/challenges/jesse-and-cookies/problem
+ */
+public class CookiesSweetness {
+
+    static int cookies(int k, int[] A) {
+        
+        return 0;
+    }
+}
