c7554a27a8c084bb6cc39bf789e330b4ebf1e8da
Raymond Chenon <raymond.chenon@gmail.com>
2019-02-06 23:38:15 +0100

    #23 add test

--- src/test/java/com/raychenon/hackerrank/tree/TreePostorderTraversalTest.java
+++ src/test/java/com/raychenon/hackerrank/tree/TreePostorderTraversalTest.java
@@ -16,13 +16,22 @@ public class TreePostorderTraversalTest {
     public void assertSingleTreePostorderTraversal() {
 
         Node node = new Node(1);
-//        node.insert(node,2);
-//        node.insert(node,5);
-//        node.insert(node,3);
-//        node.insert(node,6);
-//        node.insert(node,4);
 
         StringBuilder outContent = TreePostorderTraversal.postOrderStr(node);
         assertEquals("1 ", outContent.toString());
+    }
+
+    @Test
+    public void assertTreePostorderTraversal() {
+
+        Node node = new Node(1);
+        node.insert(node,2);
+        node.insert(node,5);
+        node.insert(node,3);
+        node.insert(node,6);
+        node.insert(node,4);
+
+        StringBuilder outContent = TreePostorderTraversal.postOrderStr(node);
+        assertEquals("4 3 6 5 2 1 ", outContent.toString());
     }
 }
