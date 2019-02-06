39daff4550033096960e8df7f016083bb5556ff2
Raymond Chenon <raymond.chenon@gmail.com>
2019-02-06 23:07:11 +0100

    #23 postorder algorithms

--- src/main/java/com/raychenon/hackerrank/tree/TreePostorderTraversal.java
+++ src/main/java/com/raychenon/hackerrank/tree/TreePostorderTraversal.java
@@ -3,14 +3,29 @@ package com.raychenon.hackerrank.tree;
 /**
  * User: raychenon
  * Date: 6/2/19
+ * https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
  */
 public class TreePostorderTraversal {
 
     public static void postOrder(Node root) {
+        if(root == null){
+            return;
+        }
 
+        //   1. Traverse the left subtree, i.e., call Postorder(left-subtree)
+        if (root.left != null){
+            postOrder(root.left);
+        }
+        //   2. Traverse the right subtree, i.e., call Postorder(right-subtree)
+        if(root.right != null){
+            postOrder(root.right);
+        }
+        //   3. Visit the root.
+
+        System.out.print(String.format("%d ",root.data));
     }
 
-    public static Node insert(Node root, int data) {
+    public static Node insert(Node root, int data){
         if(root == null) {
             return new Node(data);
         } else {
