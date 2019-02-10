de77fd6d7f20df89e2eeb76448af3d341f450872
Raymond Chenon <raymond.chenon@gmail.com>
2019-02-10 22:40:51 +0100

    #28 add more test coverage

--- src/test/kotlin/com/raychenon/kotlin/hackerrank/tree/TreePreorderTraversalTest.kt
+++ src/test/kotlin/com/raychenon/kotlin/hackerrank/tree/TreePreorderTraversalTest.kt
@@ -22,6 +22,42 @@ class TreePreorderTraversalTest {
         assertPreOrderNode("11 ", singleNode)
     }
 
+
+    @Test
+    fun assertTreePreOrderTraversal_testcase_0() {
+
+        val node = Node(1)
+        node.insert(node, 2)
+        node.insert(node, 5)
+        node.insert(node, 3)
+        node.insert(node, 6)
+        node.insert(node, 4)
+
+        assertPreOrderNode("1 2 5 3 4 6 ", node)
+    }
+    
+    @Test
+    fun assertTreePreOrderTraversal_testcase_1() {
+
+        val node = Node(1)
+        node.insert(node, 14)
+        node.insert(node, 3)
+        node.insert(node, 7)
+        node.insert(node, 4)
+        node.insert(node, 5)
+        node.insert(node, 15)
+        node.insert(node, 6)
+        node.insert(node, 13)
+        node.insert(node, 10)
+        node.insert(node, 11)
+        node.insert(node, 2)
+        node.insert(node, 12)
+        node.insert(node, 8)
+        node.insert(node, 9)
+
+        assertPreOrderNode("1 14 3 2 7 4 5 6 13 10 8 9 11 12 15 ", node)
+    }
+
     fun assertPreOrderNode(expectedValue: String, node: Node?): Unit {
 
         Assert.assertEquals(expectedValue, TreePreorderTraversalKotlin.preOrderRecursive(node).toString())
