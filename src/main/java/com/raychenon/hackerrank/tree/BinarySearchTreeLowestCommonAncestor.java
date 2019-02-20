ccdf57929f4ad48604edb8df78674919ed12c65b
Raymond Chenon <raymond.chenon@gmail.com>
2019-02-20 22:56:51 +0100

    #31 start lowest common ancestor

---
+++ src/main/java/com/raychenon/hackerrank/tree/BinarySearchTreeLowestCommonAncestor.java
@@ -0,0 +1,20 @@
+package com.raychenon.hackerrank.tree;
+
+/**
+ * User: raychenon
+ * Date: 20/2/19
+ * https://www.hackerrank.com/challenges/binary-search-tree-lowest-common-ancestor/problem
+ */
+public class BinarySearchTreeLowestCommonAncestor {
+
+    /*
+    class Node
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+    public static Node lca(Node root, int v1, int v2) {
+        // Write your code here.
+    }
+    
+}
