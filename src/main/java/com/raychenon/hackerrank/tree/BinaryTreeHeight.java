328f6f8d0c9a9e2998f08cd4ac51d2bad19122cc
Raymond Chenon <raymond.chenon@gmail.com>
2019-03-03 22:42:54 +0100

    #35 start binary tree height

---
+++ src/main/java/com/raychenon/hackerrank/tree/BinaryTreeHeight.java
@@ -0,0 +1,14 @@
+package com.raychenon.hackerrank.tree;
+
+/**
+ * User: raychenon
+ * Date: 3/3/19
+ * https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem
+ */
+public class BinaryTreeHeight {
+
+    public static int height(Node root) {
+        return 0;
+    }
+
+}
