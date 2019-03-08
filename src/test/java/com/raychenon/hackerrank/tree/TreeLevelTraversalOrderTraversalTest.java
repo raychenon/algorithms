b483d704a0e6f9b88a3ae7218567d84ff4aad5f4
Raymond Chenon <raymond.chenon@gmail.com>
2019-03-08 23:31:50 +0100

    #30 complete with more tests on Tree Order Traversal ( Breadth First Search )

--- src/test/java/com/raychenon/hackerrank/tree/TreeLevelTraversalOrderTraversalTest.java
+++ src/test/java/com/raychenon/hackerrank/tree/TreeLevelTraversalOrderTraversalTest.java
@@ -13,6 +13,12 @@ public class TreeLevelTraversalOrderTraversalTest {
 
 
     @Test
+    public void assertEmptyTreeLevelOrder() {
+        Node node = null;
+        assertEquals("", TreeLevelTraversalOrder.levelOrder(node));
+    }
+
+    @Test
     public void assertTreeLevelOrder() {
 
         Node node = new Node(1);
@@ -34,4 +40,37 @@ public class TreeLevelTraversalOrderTraversalTest {
 
         assertEquals("1 2 5 3 6 4", TreeLevelTraversalOrder.levelOrder(node));
     }
+
+
+    @Test
+    public void assertLargeTreeLevelOrder() {
+
+        Node node = new Node(20);
+        node.insert(node, 10);
+        node.insert(node, 30);
+        node.insert(node, 5);
+        node.insert(node, 15);
+        node.insert(node, 25);
+        node.insert(node, 35);
+        node.insert(node, 1);
+        node.insert(node, 7);
+        node.insert(node, 12);
+        node.insert(node, 17);
+        node.insert(node, 22);
+        node.insert(node, 27);
+        node.insert(node, 33);
+        node.insert(node, 37);
+
+        //               20
+        //        /             \
+        //       10               30
+        //     /    \           /     \
+        //    5      15        25      35
+        //   / \     /  \      / \    /  \
+        //   1  7   12  17    22  27  33  37
+        //
+
+        assertEquals("20 10 30 5 15 25 35 1 7 12 17 22 27 33 37", TreeLevelTraversalOrder.levelOrder(node));
+    }
+
 }
