d438320801b9fa6423a968dce42761c930917a92
Raymond Chenon <raymond.chenon@gmail.com>
2019-03-09 22:08:59 +0100

    #30 Tree level order traversal BFS . Add large tree as test

--- src/test/kotlin/com/raychenon/kotlin/hackerrank/tree/TreeLevelTraversalOrderTest.kt
+++ src/test/kotlin/com/raychenon/kotlin/hackerrank/tree/TreeLevelTraversalOrderTest.kt
@@ -40,5 +40,35 @@ class TreeLevelTraversalOrderTest {
         Assert.assertEquals("1 2 5 3 6 4", TreeLevelTraversalOrderKotlin.levelOrder(node))
     }
 
+    @Test
+    fun assertLargeTreeLevelOrder() {
+
+        val node = Node(20)
+        node.insert(node, 10)
+        node.insert(node, 30)
+        node.insert(node, 5)
+        node.insert(node, 15)
+        node.insert(node, 25)
+        node.insert(node, 35)
+        node.insert(node, 1)
+        node.insert(node, 7)
+        node.insert(node, 12)
+        node.insert(node, 17)
+        node.insert(node, 22)
+        node.insert(node, 27)
+        node.insert(node, 33)
+        node.insert(node, 37)
+
+        //               20
+        //        /             \
+        //       10               30
+        //     /    \           /     \
+        //    5      15        25      35
+        //   / \     /  \      / \    /  \
+        //   1  7   12  17    22  27  33  37
+        //
+
+        Assert.assertEquals("20 10 30 5 15 25 35 1 7 12 17 22 27 33 37", TreeLevelTraversalOrderKotlin.levelOrder(node))
+    }
 
 }
