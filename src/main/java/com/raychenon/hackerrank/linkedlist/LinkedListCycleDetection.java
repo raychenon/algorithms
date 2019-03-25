1396362c118d6f4147fe2a5d8427f81ae602f597
Raymond Chenon <raymond.chenon@gmail.com>
2019-03-25 22:44:02 +0100

    #21 find if the linked list is a cycle with slow and fast pointers method

--- src/main/java/com/raychenon/hackerrank/linkedlist/LinkedListCycleDetection.java
+++ src/main/java/com/raychenon/hackerrank/linkedlist/LinkedListCycleDetection.java
@@ -3,7 +3,45 @@ package com.raychenon.hackerrank.linkedlist;
 /**
  * User: raychenon
  * Date: 25/3/19
- * Time: 10:27 PM
+ * https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem
  */
-public class LinkedListCycleDetection {
+public final class LinkedListCycleDetection {
+
+    class SinglyLinkedListNode {
+        int data;
+        SinglyLinkedListNode next;
+    }
+
+    private static final int LIST_SIZE = 1000;
+
+    /**
+     * Slow and fast pointers
+     * https://en.wikipedia.org/wiki/Cycle_detection#Floyd.27s_Tortoise_and_Hare
+     * In one cycle, the Hare does 2 steps,the Tortoise does 1 step.
+     * At some point, they will reach the same index in the List.
+     *
+     * @param head
+     * @return boolean
+     */
+    static boolean hasCycle(SinglyLinkedListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        SinglyLinkedListNode tortoise = head;
+        SinglyLinkedListNode hare = head.next;
+
+        int count = 0;
+        while (tortoise != hare || count < LIST_SIZE) {
+            if (hare == null || hare.next == null) {
+                return false;
+            }
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+            count++;
+        }
+
+        return true;
+    }
+
 }
