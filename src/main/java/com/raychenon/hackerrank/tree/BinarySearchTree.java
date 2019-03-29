952ccbf8ed886fa4986478d216a278dd236de114
Raymond Chenon <raymond.chenon@gmail.com>
2019-03-29 22:21:19 +0100

    #36 start isBinarySearchTree

---
+++ src/main/java/com/raychenon/hackerrank/tree/BinarySearchTree.java
@@ -0,0 +1,14 @@
+package com.raychenon.hackerrank.tree;
+
+/**
+ * User: raychenon
+ * Date: 29/3/19
+ * https://www.hackerrank.com/challenges/is-binary-search-tree/problem
+ */
+public class BinarySearchTree {
+
+    boolean checkBST(Node root) {
+
+    }
+    
+}
