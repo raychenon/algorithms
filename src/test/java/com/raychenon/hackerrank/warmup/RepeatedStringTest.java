1b0f07c79638d0a667b702cbcd442e74c1eb0094
Raymond Chenon <raymond.chenon@gmail.com>
2019-04-13 14:33:53 +0200

    #52 test

---
+++ src/test/java/com/raychenon/hackerrank/warmup/RepeatedStringTest.java
@@ -0,0 +1,38 @@
+package com.raychenon.hackerrank.warmup;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * User: raychenon
+ * Date: 13/4/19
+ * https://www.hackerrank.com/challenges/repeated-string/problem
+ */
+public class RepeatedStringTest {
+
+//    private static RepeatedString repeatedString;
+//
+//    @BeforeClass
+//    public static void setUp() {
+//        repeatedString = new RepeatedString();
+//    }
+
+
+    @Test
+    public void assertforLetterARepeatedXTimes() {
+        assertEquals(RepeatedString.repeatedString("a", 10000000000000L), 10000000000000L);
+    }
+
+    @Test
+    public void assertForStrinNotContainingA() {
+        assertEquals(RepeatedString.repeatedString("bcdf", 10000000000000L), 0L);
+    }
+    
+    @Test
+    public void assertForStringLengthWithRemainder() {
+        assertEquals(RepeatedString.repeatedString("aba", 10L), 7L);
+    }
+
+
+}
