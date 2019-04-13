97b559142d3e957d73d526547eb438b4568ce52e
Raymond Chenon <raymond.chenon@gmail.com>
2019-04-13 14:42:26 +0200

    #52 increase code coverage in codecov.io by using non static method use instance

--- src/test/java/com/raychenon/hackerrank/warmup/RepeatedStringTest.java
+++ src/test/java/com/raychenon/hackerrank/warmup/RepeatedStringTest.java
@@ -22,17 +22,17 @@ public class RepeatedStringTest {
 
     @Test
     public void assertforLetterARepeatedXTimes() {
-        assertEquals(RepeatedString.repeatedString("a", 10000000000000L), 10000000000000L);
+        assertEquals(repeatedString.repeatedString("a", 10000000000000L), 10000000000000L);
     }
 
     @Test
     public void assertForStrinNotContainingA() {
-        assertEquals(RepeatedString.repeatedString("bcdf", 10000000000000L), 0L);
+        assertEquals(repeatedString.repeatedString("bcdf", 10000000000000L), 0L);
     }
-    
+
     @Test
     public void assertForStringLengthWithRemainder() {
-        assertEquals(RepeatedString.repeatedString("aba", 10L), 7L);
+        assertEquals(repeatedString.repeatedString("aba", 10L), 7L);
     }
 
 
