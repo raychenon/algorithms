819b39b8c4913df8316b74901854f96d3112a8d0
Raymond Chenon <raymond.chenon@gmail.com>
2019-04-16 23:50:13 +0200

    #51 start counting valleys

---
+++ src/main/java/com/raychenon/hackerrank/warmup/CountingValleys.java
@@ -0,0 +1,13 @@
+package com.raychenon.hackerrank.warmup;
+
+/**
+ * User: raychenon
+ * Date: 16/4/19
+ * https://www.hackerrank.com/challenges/counting-valleys/problem
+ */
+public class CountingValleys {
+
+    static int countingValleys(int n, String s) {
+        return 0;
+    }
+}
