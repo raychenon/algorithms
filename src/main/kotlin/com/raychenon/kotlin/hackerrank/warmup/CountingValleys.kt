ecc4fe6828eda300e0cd1d2d438dba7985aa9a0f
Raymond Chenon <raymond.chenon@gmail.com>
2019-04-18 00:35:26 +0200

    #51 solution to count the valley

--- src/main/kotlin/com/raychenon/kotlin/hackerrank/warmup/CountingValleys.kt
+++ src/main/kotlin/com/raychenon/kotlin/hackerrank/warmup/CountingValleys.kt
@@ -8,7 +8,35 @@ package com.raychenon.kotlin.hackerrank.warmup
 object CountingValleys {
 
     fun countingValleys(n: Int, s: String): Int {
-//        A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
-        return 0
+        //  A valley is a sequence of consecutive steps below sea level,
+        //  starting with a step down from sea level and ending with a step up to sea level.
+        //      _/\      _
+        //         \    /
+        //          \/\/
+
+        var nbValleys: Int = 0
+        var level: Int = 0
+        var wasBelowSeaLeval: Boolean = false
+        for (c in s.toCharArray()) {
+            level = level + valueOfStep(c)
+            print("level $c , accumulated level = $level \n")
+            if (level < 0) {
+                wasBelowSeaLeval = true
+            }
+            if (wasBelowSeaLeval && level >= 0) {
+                wasBelowSeaLeval = false
+                nbValleys = nbValleys + 1
+            }
+        }
+
+        return nbValleys
+    }
+
+    internal fun valueOfStep(c: Char): Int {
+        when (c) {
+            'D' -> return -1
+            'U' -> return 1
+            else -> return 0
+        }
     }
 }
