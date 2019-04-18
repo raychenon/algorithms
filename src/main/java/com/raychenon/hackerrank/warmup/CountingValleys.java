4bad8e5d25e14cd6a9c5b78c4c4a43dbf1cc1053
Raymond Chenon <raymond.chenon@gmail.com>
2019-04-18 23:49:36 +0200

    #51 solve the counting valleys

--- src/main/java/com/raychenon/hackerrank/warmup/CountingValleys.java
+++ src/main/java/com/raychenon/hackerrank/warmup/CountingValleys.java
@@ -8,6 +8,41 @@ package com.raychenon.hackerrank.warmup;
 public class CountingValleys {
 
     static int countingValleys(int n, String s) {
-        return 0;
+        
+        //  A valley is a sequence of consecutive steps below sea level,
+        //  starting with a step down from sea level and ending with a step up to sea level.
+        //      _/\      _
+        //         \    /
+        //          \/\/
+
+        int nbValleys = 0;
+        int level = 0;
+        boolean wasBelowSeaLevel = false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            level = level + valueOfStep(c);
+            if (level < 0) {
+                wasBelowSeaLevel = true;
+            }
+
+            if (wasBelowSeaLevel && level >= 0) {
+                nbValleys++;
+                wasBelowSeaLevel = false;
+            }
+
+        }
+        return nbValleys;
+    }
+
+    private static int valueOfStep(Character c) {
+        switch (c) {
+            case 'U':
+                return -1;
+            case 'D':
+                return +1;
+            default:
+                return 0;
+        }
     }
 }
