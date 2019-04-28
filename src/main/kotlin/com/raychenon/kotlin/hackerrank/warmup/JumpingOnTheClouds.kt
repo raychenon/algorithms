b7674d02004035414bdab82d9d4db3a4f1387c3e
Raymond Chenon <raymond.chenon@gmail.com>
2019-04-28 22:54:17 +0200

    #56 in Kotlin

---
+++ src/main/kotlin/com/raychenon/kotlin/hackerrank/warmup/JumpingOnTheClouds.kt
@@ -0,0 +1,21 @@
+package com.raychenon.kotlin.hackerrank.warmup
+
+/**
+ * User: raychenon
+ * Date: 28/4/19
+ * https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+ */
+object JumpingOnTheClouds {
+
+    fun jumpingOnClouds(c: Array<Int>): Int {
+
+        var nbJump: Int = 0
+
+        for(cloudIndex in c){
+
+        }
+
+        return nbJump
+    }
+
+}
