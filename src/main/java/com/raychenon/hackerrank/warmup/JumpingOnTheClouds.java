c012278d99116de10a7d9f2f838b027044fa3070
Raymond Chenon <raymond.chenon@gmail.com>
2019-04-28 22:41:59 +0200

    #56 start with new exercise

---
+++ src/main/java/com/raychenon/hackerrank/warmup/JumpingOnTheClouds.java
@@ -0,0 +1,14 @@
+package com.raychenon.hackerrank.warmup;
+
+/**
+ * User: raychenon
+ * Date: 28/4/19
+ * https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+ */
+public class JumpingOnTheClouds {
+
+    static int jumpingOnClouds(int[] c) {
+
+        return 0;
+    }
+}
