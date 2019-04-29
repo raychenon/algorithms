33940cebf7e4f5b3a4cefc085cb697c05c0ca347
Raymond Chenon <raymond.chenon@gmail.com>
2019-04-29 22:47:55 +0200

    #56 implement solution

--- src/main/java/com/raychenon/hackerrank/warmup/JumpingOnTheClouds.java
+++ src/main/java/com/raychenon/hackerrank/warmup/JumpingOnTheClouds.java
@@ -7,8 +7,21 @@ package com.raychenon.hackerrank.warmup;
  */
 public class JumpingOnTheClouds {
 
+    /**
+     * Jump 1 or 2 steps on the cloud ( marked by 0).
+     * The thunder clouds (marked by 1) have to be avoided.
+     */
     static int jumpingOnClouds(int[] c) {
-
-        return 0;
+        int i = 0;
+        int nbJump = 0;
+        while (i < c.length - 1) {
+            if (c[i] == 0) {
+                i++;
+            }
+            nbJump++;
+            // always iterate by at least 1
+            i++;
+        }
+        return nbJump;
     }
 }
