e4c0560f8ebca5f5c3b7a01249f3fabf9837e589
Raymond Chenon <raymond.chenon@gmail.com>
2019-04-29 22:44:00 +0200

    # test in Java

---
+++ src/test/java/com/raychenon/hackerrank/warmup/JumpingOnTheCloudsTest.java
@@ -0,0 +1,29 @@
+package com.raychenon.hackerrank.warmup;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * User: raychenon
+ * Date: 2019-04-29
+ * https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+ */
+public class JumpingOnTheCloudsTest {
+
+    @Test
+    public void assertSample0() {
+        assertEquals(4, JumpingOnTheClouds.jumpingOnClouds(new int[]{0, 0, 1, 0, 0, 1, 0}));
+    }
+
+    @Test
+    public void assertSample1() {
+        assertEquals(3, JumpingOnTheClouds.jumpingOnClouds(new int[]{0, 0, 0, 1, 0, 0}));
+    }
+
+    @Test
+    public void assertOnlyCloudsSizeEven() {
+        assertEquals(3, JumpingOnTheClouds.jumpingOnClouds(new int[]{0, 0, 0, 0, 0, 0}));
+    }
+
+}
