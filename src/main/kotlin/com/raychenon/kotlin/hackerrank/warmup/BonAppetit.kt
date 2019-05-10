86586fae2d472a16af626f48cf6f0b05fb46d1a3
Raymond Chenon <raymond.chenon@gmail.com>
2019-05-10 23:33:58 +0200

    #54 init Bon Appétit

---
+++ src/main/kotlin/com/raychenon/kotlin/hackerrank/warmup/BonAppetit.kt
@@ -0,0 +1,15 @@
+package com.raychenon.kotlin.hackerrank.warmup
+
+/**
+ * User: raychenon
+ * Date: 2019-05-10
+ * https://www.hackerrank.com/challenges/bon-appetit/problem
+ */
+object BonAppetit {
+
+    fun bonAppetit(bill: Array<Int>, k: Int, b: Int): Unit {
+
+
+    }
+    
+}
