9773d0385b1bae2ebebde7fb33442c50d2814f99
Raymond Chenon <raymond.chenon@gmail.com>
2019-05-11 23:40:54 +0200

    #54 split the bill

--- src/main/kotlin/com/raychenon/kotlin/hackerrank/warmup/BonAppetit.kt
+++ src/main/kotlin/com/raychenon/kotlin/hackerrank/warmup/BonAppetit.kt
@@ -8,8 +8,23 @@ package com.raychenon.kotlin.hackerrank.warmup
 object BonAppetit {
 
     fun bonAppetit(bill: Array<Int>, k: Int, b: Int): Unit {
-
-
+        var result = calculateBillCharged(bill, k, b)
+        print("$result")
     }
-    
+
+    fun calculateBillCharged(bill: Array<Int>, indexNotBilled: Int, amountCharged: Int): String {
+        var amount = 0
+        for ((index, foodCost) in bill.withIndex()) {
+            if (index != indexNotBilled) {
+                amount = amount + foodCost
+            }
+        }
+
+        amount = amount / 2
+        if (amount == amountCharged) {
+            return "Bon Appetit"
+        } else {
+            return "$amount"
+        }
+    }
 }
