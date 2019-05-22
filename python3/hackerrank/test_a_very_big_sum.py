0759122e5f1b701a0ed242b7b34b6ff4f7e07754
Raymond Chenon <raymond.chenon@gmail.com>
2019-05-22 23:54:46 +0200

    #39 separate the tests in test*.py file

---
+++ python3/hackerrank/test_a_very_big_sum.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+from a_very_big_sum import aVeryBigSum
+import unittest2
+
+
+"""
+https://www.hackerrank.com/challenges/a-very-big-sum/problem
+"""
+class Test_Sum(unittest2.TestCase):
+    def test_sum(self):
+        self.assertEquals(aVeryBigSum([4, 5]), 9)
+
+    def test_larger_sum(self):
+        self.assertEquals(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]), 5000000015)
+
+
+if __name__ == '__main__':
+    unittest2.main()
