12d8218df1c806540ea46b5ff4233abee59db613
Raymond Chenon <raymond.chenon@gmail.com>
2019-05-26 11:05:19 +0200

    #10 add tests in python3 for Ransom Note

---
+++ python3/hackerrank/test_ctci_ransom_note.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+
+import unittest2 as unittest
+
+from ctci_ransom_note import checkMagazine
+
+
+class Test_ransom_note(unittest.TestCase):
+
+    def test_ransom_note_is_valid(self):
+        self.assertEquals(
+            checkMagazine(['give', 'me', 'one', 'grand', 'today', 'night'], ['give', 'one', 'grand', 'today']), True)
+
+    def test_ransom_note_is_equal_to_magazine(self):
+        self.assertEquals(checkMagazine(['some'], ['some']), True)
+
+    def test_ransom_smaller_than_magazine(self):
+        self.assertEquals(checkMagazine(['catch', 'me', 'if', 'you', 'can'], ['catch', 'me']), True)
+
+    def test_ransom_case_sensitive(self):
+        self.assertEquals(checkMagazine(['me'], ['Me']), False)
+
+    def test_ransom_empty(self):
+        self.assertEquals(checkMagazine(['me'], []), False)
+
+    def test_ransom_2(self):
+        self.assertEquals(
+            checkMagazine(["two", "times", "three", "is", "not", "four"], ["two", "times", "two", "is", "four"]), False)
+
+    def test_ransom_3(self):
+        self.assertEquals(
+            checkMagazine(["ive", "got", "a", "lovely", "bunch", "of", "coconuts"], ["ive", "got", "some", "coconuts"]),
+            False)
+
+
+if __name__ == '__main__':
+    unittest.main()
