50e8a532dd3754fe69025fe88acab151135343f4
Raymond Chenon <raymond.chenon@gmail.com>
2019-05-26 21:44:42 +0200

    #10 add another test

--- python3/hackerrank/test_ctci_ransom_note.py
+++ python3/hackerrank/test_ctci_ransom_note.py
@@ -5,6 +5,10 @@ import unittest2 as unittest
 
 from ctci_ransom_note import checkMagazine
 
+""""
+https://www.hackerrank.com/challenges/ctci-ransom-note/problem
+"""
+
 
 class Test_ransom_note(unittest.TestCase):
 
@@ -23,6 +27,14 @@ class Test_ransom_note(unittest.TestCase):
 
     def test_ransom_empty(self):
         self.assertEquals(checkMagazine(['me'], []), True)
+
+    def test_ransom_1(self):
+        self.assertEquals(
+            checkMagazine(
+                ["o", "l", "x", "imjaw", "bee", "khmla", "v", "o", "v", "o", "imjaw", "l", "khmla", "imjaw", "x"],
+                ["imjaw", "l", "khmla", "x", "imjaw", "o", "l", "l", "o", "khmla", "v", "bee", "o", "o", "imjaw",
+                 "imjaw", "o"]),
+            False)
 
     def test_ransom_2(self):
         self.assertEquals(
