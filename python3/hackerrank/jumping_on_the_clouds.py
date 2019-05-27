b255a4b5e7b0ea99f2da9a14e0f53d3b342ff706
Raymond Chenon <raymond.chenon@gmail.com>
2019-05-27 23:34:21 +0200

    #56 version in Python

---
+++ python3/hackerrank/jumping_on_the_clouds.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+"""
+https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+Jump 1 or 2 steps on the cloud ( marked by 0).
+The thunder clouds (marked by 1) have to be avoided.
+"""
+
+
+def jumpingOnClouds(c):
+    i = 0
+    nbJump = 0
+    while (i < len(c)):
+        if (c[i] == 0):
+            i += 1
+        nbJump += 1
+        i += 1
+    return nbJump
