ab64e8b2a7d001b3cd3e0daab4c57d803e8b98bc
Raymond Chenon <raymond.chenon@gmail.com>
2019-06-27 23:28:49 +0200

    #65 adds ListNode

--- src/main/java/com/raychenon/leetcode/AddTwoNumbers.java
+++ src/main/java/com/raychenon/leetcode/AddTwoNumbers.java
@@ -8,14 +8,14 @@ import java.util.List;
  * User: raychenon
  * Date: 2019-06-27
  * https://leetcode.com/problems/add-two-numbers/
- *
+ * <p>
  * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
- *
+ * <p>
  * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
  */
 public class AddTwoNumbers {
 
-    public List<Integer> addTwoNumbers(List<Integer> l1, List<Integer> l2) {
+    public List<Integer> addTwoNumbersList(List<Integer> l1, List<Integer> l2) {
 
         List<Integer> result = new LinkedList<>();
         int carryNext = 0;
@@ -36,12 +36,45 @@ public class AddTwoNumbers {
         return result;
     }
 
-    public static class ListNode {
-        int val;
-        ListNode next;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        ListNode(int x) {
-            val = x;
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
         }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+        
+//        ListNode head = new ListNode(0);
+//        ListNode curr = head;
+//        int carry = 0;
+//        while (l1.next != null || l2.next != null) {
+//
+//            int sum = carry + l1.val + l2.val;
+//            carry = sum / 10;
+//
+//            curr.next = new ListNode(sum % 10);
+//
+//            curr = curr.next;
+//            l1 = l1.next;
+//            l2 = l2.next;
+//        }
+//        if (carry > 0) {
+//            curr.next = new ListNode(carry);
+//        }
+//
+//        return head.next;
     }
+
 }
--- src/test/java/com/raychenon/leetcode/AddTwoNumbersTest.java
+++ src/test/java/com/raychenon/leetcode/AddTwoNumbersTest.java
@@ -4,6 +4,8 @@ import com.sun.tools.javac.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.Collections;
+
 import static junit.framework.TestCase.assertEquals;
 
 /**
@@ -21,9 +23,39 @@ public class AddTwoNumbersTest {
         addTwoNumbers = new AddTwoNumbers();
     }
 
+ 
     @Test
     public void assertSumSameListSize() {
 
-        assertEquals(List.of(8, 0, 7), addTwoNumbers.addTwoNumbers(List.of(3, 4, 2), List.of(4, 6, 5)));
+        assertEquals(List.of(8, 0, 7), addTwoNumbers.addTwoNumbersList(List.of(3, 4, 2), List.of(4, 6, 5)));
+       
+        compareListNode(createListNode(List.of(7,0,8)), addTwoNumbers.addTwoNumbers(createListNode(List.of(3,4,2)),createListNode(List.of(4,6,5))));
+    }
+
+    @Test
+    public void assertCompareListNode() {
+        compareListNode(createListNode(List.of(1, 2,3)), createListNode(List.of(1,2)));
+    }
+
+    private void compareListNode(ListNode l1, ListNode l2) {
+        while (l1 != null || l2 != null) {
+
+            System.out.println("L1 = " + l1.val + " , L2 = " + l2.val);
+            assertEquals(l1.val, l2.val);
+
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+    }
+
+    private ListNode createListNode(List<Integer> list) {
+        ListNode head = new ListNode(0);
+        ListNode node = head;
+        for (Integer e : list) {
+            node.val = e;
+            node.next = new ListNode(e);
+            node = node.next;
+        }
+        return head;
     }
 }
