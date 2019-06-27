8a789f1e01479536a38b3d6643cc6a8d0ff01918
Raymond Chenon <raymond.chenon@gmail.com>
2019-06-28 00:20:50 +0200

    #65 fix. Use varargs correctly with a first parameter

--- src/test/java/com/raychenon/leetcode/AddTwoNumbersTest.java
+++ src/test/java/com/raychenon/leetcode/AddTwoNumbersTest.java
@@ -1,10 +1,9 @@
 package com.raychenon.leetcode;
 
 import com.sun.tools.javac.util.List;
+import org.jetbrains.annotations.NotNull;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.util.Collections;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -23,39 +22,50 @@ public class AddTwoNumbersTest {
         addTwoNumbers = new AddTwoNumbers();
     }
 
- 
+
     @Test
     public void assertSumSameListSize() {
 
         assertEquals(List.of(8, 0, 7), addTwoNumbers.addTwoNumbersList(List.of(3, 4, 2), List.of(4, 6, 5)));
-       
-        compareListNode(createListNode(List.of(7,0,8)), addTwoNumbers.addTwoNumbers(createListNode(List.of(3,4,2)),createListNode(List.of(4,6,5))));
+
+        compareListNode(createListNode(7, 0, 8), addTwoNumbers.addTwoNumbers(createListNode(3, 4, 2), createListNode(4, 6, 5)));
     }
 
     @Test
     public void assertCompareListNode() {
-        compareListNode(createListNode(List.of(1, 2,3)), createListNode(List.of(1,2)));
+        compareListNode(createListNode(1, 2, 3), createListNode(1, 2));
     }
 
     private void compareListNode(ListNode l1, ListNode l2) {
         while (l1 != null || l2 != null) {
 
-            System.out.println("L1 = " + l1.val + " , L2 = " + l2.val);
-            assertEquals(l1.val, l2.val);
+            int val1 = (l1 != null) ? l1.val : -1;
+            int val2 = (l2 != null) ? l2.val : -1;
+
+            System.out.println(String.format("L1 = %d , L2 = %d", val1, val2));
+
+            assertEquals(val1, val2);
 
             l1 = l1.next;
             l2 = l2.next;
         }
     }
 
-    private ListNode createListNode(List<Integer> list) {
-        ListNode head = new ListNode(0);
+    private ListNode createListNode(@NotNull int value, int... a) {
+        ListNode head = new ListNode(value);
         ListNode node = head;
-        for (Integer e : list) {
-            node.val = e;
+        for (int e : a) {
             node.next = new ListNode(e);
             node = node.next;
         }
+
+//        ListNode curr = head;
+//        System.out.println("------- createListNode --------");
+//        while (curr != null) {
+//            System.out.println("Curr = " + curr.val);
+//            curr = curr.next;
+//        }
+
         return head;
     }
 }
