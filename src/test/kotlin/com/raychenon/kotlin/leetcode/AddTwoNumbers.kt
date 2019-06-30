88af5482d02f30eb5d81c03b934a2ec66ad6a216
Raymond Chenon <raymond.chenon@gmail.com>
2019-06-30 21:43:13 +0200

    # kotlin progress

--- src/test/kotlin/com/raychenon/kotlin/leetcode/AddTwoNumbers.kt
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/AddTwoNumbers.kt
@@ -3,7 +3,11 @@ package com.raychenon.kotlin.leetcode
 /**
  * User: raychenon
  * Date: 2019-06-29
- * Time: 23:59
+ * https://leetcode.com/problems/add-two-numbers/
  */
 class AddTwoNumbers {
+
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+
+    }
 }
---
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/ListNode.kt
@@ -0,0 +1,10 @@
+package com.raychenon.kotlin.leetcode
+
+/**
+ * User: raychenon
+ * Date: 2019-06-30
+ * Time: 21:42
+ */
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
