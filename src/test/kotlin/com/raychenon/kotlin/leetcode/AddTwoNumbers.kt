e20c3bc0ce1c4189790d103068ee7b3e2d046738
Raymond Chenon <raymond.chenon@gmail.com>
2019-07-02 23:55:51 +0200

    #65 ad two list in kotlin

--- src/test/kotlin/com/raychenon/kotlin/leetcode/AddTwoNumbers.kt
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/AddTwoNumbers.kt
@@ -8,6 +8,19 @@ package com.raychenon.kotlin.leetcode
 class AddTwoNumbers {
 
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        val head = ListNode(0)
+        var carry: Int = 0
+        var curr = head
 
+        while (l1?.next != null && l2?.next != null) {
+            val x = l1?.`val` ?: 0
+            val y = l2?.`val` ?: 0
+            val sum = carry + x + y
+            carry = sum / 100
+            curr.next = ListNode(sum % 10)
+            curr = curr.next!!
+        }
+
+        return head.next
     }
 }
