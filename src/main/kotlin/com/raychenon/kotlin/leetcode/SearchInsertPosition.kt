12abf8e67c548f9b54b22b25f93fb0ab09f9daf5
Raymond Chenon <raymond.chenon@gmail.com>
2019-07-13 14:58:54 +0200

    #69 start searchInsert

---
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/SearchInsertPosition.kt
@@ -0,0 +1,34 @@
+package com.raychenon.kotlin.leetcode
+
+/**
+ * User: raychenon
+ * Date: 2019-07-13
+ * https://leetcode.com/problems/search-insert-position/
+ */
+object SearchInsertPosition {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+
+        // edge limit
+        val min = nums.get(0)
+        val max = nums.get(nums.size - 1)
+        if (target < min) {
+            return 0
+        } else if (target > max) {
+            return nums.size
+        }
+        
+        // general
+        for ((i, v) in nums.withIndex()) {
+            // found
+            if (v == target) {
+                return i
+            } else if (v > target) {
+                // target not found , between numbers
+                if ((i - 1) > 0 && nums.get(i - 1) < target) {
+                    return i
+                }
+            }
+        }
+        return nums.size
+    }
+}
