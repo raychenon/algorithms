405cd3b0a68f34652c68bbaef0f237b78d28c70f
Raymond Chenon <raymond.chenon@gmail.com>
2019-08-08 23:08:40 +0200

    #67 isSudokuValid. Add tests

---
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/ValidSudoku.kt
@@ -0,0 +1,18 @@
+package com.raychenon.kotlin.leetcode
+
+/**
+ * User: raychenon
+ * Date: 2019-08-08
+ * https://leetcode.com/problems/valid-sudoku/
+ */
+object ValidSudoku {
+
+    /**
+     * Each row must contain the digits 1-9 without repetition.
+     * Each column must contain the digits 1-9 without repetition.
+     * Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+     */
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+       return true
+    }
+}
---
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/ValidSudokuTest.kt
@@ -0,0 +1,44 @@
+package com.raychenon.kotlin.leetcode
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * User: raychenon
+ * Date: 2019-08-08
+ * https://leetcode.com/problems/valid-sudoku/
+ */
+class ValidSudokuTest {
+
+    @Test
+    fun isExample1Valid() {
+        val grid: Array<CharArray> = arrayOf(
+            charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+            charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+        )
+        Assert.assertEquals(true, ValidSudoku.isValidSudoku(grid))
+    }
+
+    @Test
+    fun isExample2Valid() {
+        val grid: Array<CharArray> = arrayOf(
+            charArrayOf('8','3','.','.','7','.','.','.','.'),
+            charArrayOf('6','.','.','1','9','5','.','.','.'),
+            charArrayOf('.','9','8','.','.','.','.','6','.'),
+            charArrayOf('8','.','.','.','6','.','.','.','3'),
+            charArrayOf('4','.','.','8','.','3','.','.','1'),
+            charArrayOf('7','.','.','.','2','.','.','.','6'),
+            charArrayOf('.','6','.','.','.','.','2','8','.'),
+            charArrayOf('.','.','.','4','1','9','.','.','5'),
+            charArrayOf('.','.','.','.','8','.','.','7','9')
+        )
+        Assert.assertEquals(true, ValidSudoku.isValidSudoku(grid))
+    }
+}
