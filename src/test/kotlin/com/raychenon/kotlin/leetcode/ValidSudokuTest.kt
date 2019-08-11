b7a9a5ab3b6f7adddf4adc866fe2ceebb2224fe3
Raymond Chenon <raymond.chenon@gmail.com>
2019-08-11 23:49:50 +0200

    #67 format

--- src/test/kotlin/com/raychenon/kotlin/leetcode/ValidSudokuTest.kt
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/ValidSudokuTest.kt
@@ -29,15 +29,15 @@ class ValidSudokuTest {
     @Test
     fun isExample2Valid() {
         val grid: Array<CharArray> = arrayOf(
-            charArrayOf('8','3','.','.','7','.','.','.','.'),
-            charArrayOf('6','.','.','1','9','5','.','.','.'),
-            charArrayOf('.','9','8','.','.','.','.','6','.'),
-            charArrayOf('8','.','.','.','6','.','.','.','3'),
-            charArrayOf('4','.','.','8','.','3','.','.','1'),
-            charArrayOf('7','.','.','.','2','.','.','.','6'),
-            charArrayOf('.','6','.','.','.','.','2','8','.'),
-            charArrayOf('.','.','.','4','1','9','.','.','5'),
-            charArrayOf('.','.','.','.','8','.','.','7','9')
+            charArrayOf('8', '3', '.', '.', '7', '.', '.', '.', '.'),
+            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+            charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
         )
         Assert.assertEquals(true, ValidSudoku.isValidSudoku(grid))
     }
