697e1496ffe8e5a7c7be66044ac29810d2a08916
Raymond Chenon <raymond.chenon@gmail.com>
2019-08-19 13:18:49 +0200

    #238 store the product results in arrays

--- src/main/java/com/raychenon/leetcode/ProductofArrayExceptSelf.java
+++ src/main/java/com/raychenon/leetcode/ProductofArrayExceptSelf.java
@@ -19,30 +19,22 @@ public class ProductofArrayExceptSelf {
         //  prodLeft  i   prodRight
         //  ---------> <-----------
         // [ | | | | | | | | | | | ]
-        int prodLeft = 1;
-        int prodRight = 1;
+        int prodLeft[] = new int[size];
+        int prodRight[] = new int[size];
 
+        prodLeft[0] = 1;
         for (int i = 1; i < size; i++) {
-            prodRight = prodRight * nums[i];
+            prodLeft[i] = prodLeft[i - 1] * nums[i - 1];
         }
-        boolean isFirst = true;
+
+        prodRight[size - 1] = 1;
+        for (int i = size - 2; i >= 0; i--) {
+            prodRight[i] = prodRight[i + 1] * nums[i + 1];
+        }
 
         for (int i = 0; i < size; i++) {
-            if (i - 1 > 0) {
-                if (nums[i - 1] != 0) {
-                    prodLeft = prodLeft * nums[i - 1];
-                }
-            }
-            if (i < size && !isFirst) {
-                if (nums[i] != 0) {
-                    prodRight = prodRight / nums[i];
-                }
-            }
-            isFirst = false;
-
-            int prod = prodLeft * prodRight;
+            int prod = prodLeft[i] * prodRight[i];
             output[i] = prod;
-            System.out.println("prod = " + prod + " , index = " + i + " , left = " + prodLeft + " , right = " + prodRight);
         }
 
         return output;
