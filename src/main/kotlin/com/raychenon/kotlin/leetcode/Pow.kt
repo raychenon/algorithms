677011310bad2799782a00493cc2dee107454e16
Raymond Chenon <raymond.chenon@gmail.com>
2019-08-28 00:12:39 +0200

    #83 run time log(n)

--- src/main/kotlin/com/raychenon/kotlin/leetcode/Pow.kt
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/Pow.kt
@@ -6,26 +6,49 @@ package com.raychenon.kotlin.leetcode
  * https://leetcode.com/problems/powx-n/
  */
 object Pow {
-    fun myPow(x: Double, n: Int): Double {
-        if (n == 0) return 1.0
+    //
+    /**
+     * Exponentiation by squaring
+     * https://en.wikipedia.org/wiki/Exponentiation_by_squaring
+     *
+     *Time Complexity: O(log(n)), where 'n' is the exponent
+     * Space Complexity: O(1)
+     */
+    fun myPow(base: Double, exp: Int): Double {
+        if (exp == 0) return 1.0
+        var n = exp
+        var x = base
+        var res = 1.0
 
-        var res = x
-        for (i in 1 until Math.abs(n)) {
-            res = res * x
-        }
         if (n < 0) {
-            res = 1 / res
+            x = 1 / x
+            n = -n
         }
-        return res
+
+        while (n > 1) {
+            // if n is odd
+            if ((n % 2) == 1) {
+                res = res * x
+                x = x * x
+                n = (n - 1) / 2
+            } else {
+                x = x * x
+                n = n / 2
+            }
+        }
+
+        return res * x
     }
 
     fun myPowRecursive(x: Double, n: Int): Double {
         if (n == 0) return 1.0
-        
+
         val temp = myPowRecursive(x, n / 2)
+        // n is even,  x ^ n = x^(n/2) * x^(n/2)
         return if (n % 2 == 0) {
             temp * temp
         } else {
+            // n is odd,  x ^ n = x * x^(n/2) * x^(n/2)
             if (n > 0)
                 x * temp * temp
             else
