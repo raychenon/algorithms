5f9f9a0aa57c17e9fad7c7b7c73f32d610c8a8f3
Raymond Chenon <raymond.chenon@gmail.com>
2019-09-10 22:36:20 +0200

    #87 Group anagrams TDD

---
+++ src/main/java/com/raychenon/leetcode/GroupAnagrams.java
@@ -0,0 +1,15 @@
+package com.raychenon.leetcode;
+
+import java.util.List;
+
+/**
+ * User: raychenon
+ * Date: 2019-09-10
+ * https://leetcode.com/problems/group-anagrams/
+ */
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+    }
+}
---
+++ src/test/java/com/raychenon/leetcode/GroupAnagramsTest.java
@@ -0,0 +1,32 @@
+package com.raychenon.leetcode;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * User: raychenon
+ * Date: 2019-09-10
+ * https://leetcode.com/problems/group-anagrams/
+ */
+public class GroupAnagramsTest {
+
+    private static GroupAnagrams groupAnagrams;
+
+    @BeforeClass
+    public static void setUp() {
+        groupAnagrams = new GroupAnagrams();
+    }
+
+    @Test
+    public void testProductOfArray() {
+        List<List<String>> expectedResult = Arrays.asList(Arrays.asList("ate", "eat", "tea"), Arrays.asList("nat", "tan"), Arrays.asList("bat"));
+        List<List<String>> result = groupAnagrams.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        assertThat(expectedResult, is(result));
+    }
+}
