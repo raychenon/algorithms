035a3f94d5a2fb80677080a4a89814ec09a94d39
Raymond Chenon <raymond.chenon@gmail.com>
2019-09-12 15:40:06 +0200

    #85 not quite setup the CI in python. But the also is OK

--- python3/algorithms/leetcode/valid_parentheses.py
+++ python3/algorithms/leetcode/valid_parentheses.py
@@ -1,7 +1,12 @@
 #!/bin/python3
+# import unittest2 as unittest
 
-
-def isValid(s: str) -> bool:
+def is_valid(s: str) -> bool:
+    """
+    https://leetcode.com/problems/valid-parentheses/
+    Time complexity : O(n) because we simply traverse the given string one character at a time and push and pop operations on a stack take O(1) time.
+    Space complexity : O(n) as we push all opening brackets onto the stack and in the worst case, we will end up pushing all the brackets onto the stack. e.g. ((((((((((
+    """
     stack = []
     mapping = {")": "(", "}": "{", "]": "["}
     opening = {"(", "[", "{"}
@@ -16,10 +21,19 @@ def isValid(s: str) -> bool:
     return not stack
 
 
+
+# class ValidParenthsesTest(unittest.TestCase):
+#     def test_prent(self):
+#         self.assertEquals(is_valid(["()"]), True)
+#
+# if __name__ == '__main__':
+#     unittest.main()
+
+
 if __name__ == '__main__':
-    assert isValid(["()"]) == True
-    assert isValid(["([{}])"]) == True
-    assert isValid(["(){}[]"]) == True
-    assert isValid(["([{}])"]) == True
-    assert isValid(["(])"]) == False
-    assert isValid(["][)("]) == False
+    assert is_valid(["()"]) == True
+    assert is_valid(["([{}])"]) == True
+    assert is_valid(["(){}[]"]) == True
+    assert is_valid(["([{}])"]) == True
+    assert is_valid(["(((((((()"]) == False
+    assert is_valid(["][)("]) == False
