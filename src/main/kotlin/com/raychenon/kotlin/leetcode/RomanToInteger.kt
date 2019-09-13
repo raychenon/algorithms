821adbf4a2249ad392d83ca246c6e5d3629a38d8
Raymond Chenon <raymond.chenon@gmail.com>
2019-09-13 11:26:32 +0200

    #90 Roman to Integer

--- src/main/kotlin/com/raychenon/kotlin/leetcode/RomanToInteger.kt
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/RomanToInteger.kt
@@ -17,13 +17,27 @@ object RomanToInteger {
         'M' to 1000
     )
 
+    /**
+     * time complexity: O(n) where n is the length of the String
+     * space complexity: 0(1) constant
+     */
     fun romanToInt(s: String): Int {
 
         var sum: Int = 0
-        for (chr in s.toCharArray()) {
-            sum = sum + romanToIntMap.getOrDefault(chr, 0)
-
+        for (i in 0..s.length - 1) {
+            val chr = s.get(i)
+            val value = convertToInt(chr)
+            // Trick: if the next Char has a bigger value then the current int value is negative
+            // ex : IV = 4 = -1+5
+            // IX = -1+10 = 9
+            val signedValue = if ((i + 1) < s.length && value < convertToInt(s.get(i + 1))) -value else value
+            sum = sum + signedValue
         }
         return sum
     }
+
+    private fun convertToInt(c: Char): Int {
+        return romanToIntMap.getOrDefault(c, 0)
+    }
+
 }
