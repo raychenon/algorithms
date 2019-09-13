66ebdd0d7ebae347849cc0dccc4af5d51a050b59
Raymond Chenon <raymond.chenon@gmail.com>
2019-09-13 11:03:22 +0200

    #90 need to add 4x,9x cases

--- src/main/kotlin/com/raychenon/kotlin/leetcode/RomanToInteger.kt
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/RomanToInteger.kt
@@ -6,7 +6,24 @@ package com.raychenon.kotlin.leetcode
  * https://leetcode.com/problems/roman-to-integer/
  */
 object RomanToInteger {
+
+    private val romanToIntMap: Map<Char, Int> = mapOf(
+        'I' to 1,
+        'V' to 5,
+        'X' to 10,
+        'L' to 50,
+        'C' to 100,
+        'D' to 500,
+        'M' to 1000
+    )
+
     fun romanToInt(s: String): Int {
-         return 0
+
+        var sum: Int = 0
+        for (chr in s.toCharArray()) {
+            sum = sum + romanToIntMap.getOrDefault(chr, 0)
+
+        }
+        return sum
     }
 }
