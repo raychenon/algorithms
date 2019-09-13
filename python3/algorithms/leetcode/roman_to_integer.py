362db8a73bbf21e2c29fe0c73a90a870f085b2b8
Raymond Chenon <raymond.chenon@gmail.com>
2019-09-13 12:47:18 +0200

    #90 fix in python3

--- python3/algorithms/leetcode/roman_to_integer.py
+++ python3/algorithms/leetcode/roman_to_integer.py
@@ -10,12 +10,15 @@ def roman_to_int(s: str) -> int:
     map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
     for i in range(len(s)):
-        if (i + 1 < len(s)-1) and map[s[i]] < map[s[i+i]]:
+        if (i + 1 < len(s)) and map[s[i]] < map[s[i + 1]]:
             sum = sum - map[s[i]]
         else:
             sum = sum + map[s[i]]
     return sum
 
+
 if __name__ == "__main__":
-    assert roman_to_int("III") == 1
+    assert roman_to_int("III") == 3
+    assert roman_to_int("LVIII") == 58
     assert roman_to_int("MCMXCIV") == 1994
+    assert roman_to_int("MMMCMXCIX") == 3999
