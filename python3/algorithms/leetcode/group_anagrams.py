793367926c412563f28aa56dd0d1f5f9d67aa136
Raymond Chenon <raymond.chenon@gmail.com>
2019-09-18 12:33:31 +0200

    #87 solution with constant space

--- python3/algorithms/leetcode/group_anagrams.py
+++ python3/algorithms/leetcode/group_anagrams.py
@@ -1,12 +1,22 @@
 #!/bin/python3
+from typing import List
+from collections import defaultdict
 
-"""
-https://leetcode.com/problems/group-anagrams/
-"""
 
-class GroupAnagrams:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        map = dict()
-        for s in strs:
-            map[tuple(sorted(s))].append(s)
-        return map.values
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    """
+    https://leetcode.com/problems/group-anagrams/
+    time complexity: O(n)
+    space complexity: O(1)
+    """
+    mapping = collections.defaultdict(list)
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        mapping[tuple(count)].append(s)
+    return mapping.values
+
+
+if __name__ == "__main__":
+    assert groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) == [["ate", "eat", "tea"], ["nat", "tan"], ["bat"]]
