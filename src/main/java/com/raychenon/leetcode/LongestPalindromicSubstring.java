c08ccb5c367bb82cb117ea15a29ec5ba0e6266e9
Raymond Chenon <raymond.chenon@gmail.com>
2019-09-29 23:55:57 +0200

    #100 Longest Palindromic Substring method signature

---
+++ src/main/java/com/raychenon/leetcode/LongestPalindromicSubstring.java
@@ -0,0 +1,12 @@
+package com.raychenon.leetcode;
+
+/**
+ * User: raychenon
+ * Date: 2019-09-29
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ */
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+
+    }
+}
---
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/LongestPalindromicSubstring.kt
@@ -0,0 +1,12 @@
+package com.raychenon.kotlin.leetcode
+
+/**
+ * User: raychenon
+ * Date: 2019-09-29
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ */
+object LongestPalindromicSubstring {
+    fun longestPalindrome(s: String): String {
+
+    }
+}
