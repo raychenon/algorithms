df58b98b87fe8f50b4d6d057395b1c1520cc2b2b
Raymond Chenon <raymond.chenon@gmail.com>
2019-10-01 00:58:28 +0200

    #100 longest palindrome  in Kotlin

--- src/main/kotlin/com/raychenon/kotlin/leetcode/LongestPalindromicSubstring.kt
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/LongestPalindromicSubstring.kt
@@ -6,7 +6,45 @@ package com.raychenon.kotlin.leetcode
  * https://leetcode.com/problems/longest-palindromic-substring/
  */
 object LongestPalindromicSubstring {
+
+
+    /**
+     * time complexity: O(n^2). Single for loop + while loop in {@link #expandAroundCenter}
+     * space complexity: O(1)
+     *
+     * @param s
+     * @return longest palindrome
+     */
     fun longestPalindrome(s: String): String {
-        return ""
+        if (s.isEmpty()) return ""
+
+        var start = 0
+        var end = 0
+        for (i in 0 until s.length) {
+            // max length between
+            // - palindrome with odd length: [ |i| ]
+            // - palindrome with even length: [ |i|i+1| ]
+            val len = Math.max(lengthFromTheCenter(s, i, i), lengthFromTheCenter(s, i, i + 1))
+            if (len > end - start) {
+                start = i - (len - 1) / 2
+                end = i + len / 2
+            }
+        }
+        return s.substring(start, end + 1)
+    }
+
+    /**
+     * \ \start\ \ \i\ \ \ \end|
+     *          <---  ----->
+     *          |--length--|
+     */
+    private fun lengthFromTheCenter(s: String, start: Int, end: Int): Int {
+        var left = start
+        var right = end
+        while (left >= 0 && right < s.length && s[left] == s[right]) {
+            left--
+            right++
+        }
+        return right - left - 1
     }
 }
