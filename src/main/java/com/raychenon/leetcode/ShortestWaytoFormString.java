9091bbd4dc5776520183b7935bd4343f77c4f51d
Raymond Chenon <raymond.chenon@gmail.com>
2019-10-14 23:25:55 +0200

    #105 rename variables

--- src/main/java/com/raychenon/leetcode/ShortestWaytoFormString.java
+++ src/main/java/com/raychenon/leetcode/ShortestWaytoFormString.java
@@ -8,10 +8,10 @@ import java.util.Arrays;
  * https://leetcode.com/problems/shortest-way-to-form-string/
  */
 public class ShortestWaytoFormString {
-    
+
     /**
      * time complexity: O(S*T) , where S: length of source and T: length of target
-     * 
+     *
      * @param source
      * @param target
      * @return
@@ -32,37 +32,38 @@ public class ShortestWaytoFormString {
     }
 
     /**
-     *  Inverted index data structure
-     *  dictIndex[i][c - 'a'] represents the earliest index >= i where character c occurs in source.
-     *  
-     * time complexity:  O(S + T) , where S: length of source and T: length of target
+     * Inverted index data structure
+     * dictIndex[i][c - 'a'] represents the earliest index >= i where character c occurs in source.
+     * <p>
+     * time complexity:  O(S+T) , where S is the length of source and T is the length of target
+     * space complexity: O(S)
+     *
      * @param source
      * @param target
      * @return
      */
     public int shortestWayInvertedIndex(String source, String target) {
-        char[] s = source.toCharArray();
-        char[] t = target.toCharArray();
-        int M = s.length;
-        int N = t.length;
-        int[][] dictIndex = new int[M][26];
-        Arrays.fill(dictIndex[M-1], -1); // -1 means no that char in source
-        dictIndex[M - 1][s[M - 1] - 'a'] = M - 1;
-        for(int x = M - 2; x >= 0; --x) {
+        char[] sArr = source.toCharArray();
+        char[] tArr = target.toCharArray();
+        int S = sArr.length;
+        int[][] dictIndex = new int[S][26];
+        Arrays.fill(dictIndex[S - 1], -1); // -1 means no that char in source
+        dictIndex[S - 1][sArr[S - 1] - 'a'] = S - 1;
+        for (int x = S - 2; x >= 0; --x) {
             dictIndex[x] = Arrays.copyOf(dictIndex[x + 1], 26);
-            dictIndex[x][s[x] - 'a'] = x;
+            dictIndex[x][sArr[x] - 'a'] = x;
         }
         int ans = 0;
         int idx = 0;
-        for(char c: t) {
-            if(dictIndex[0][c - 'a'] == -1) return -1;
-            if(dictIndex[idx][c - 'a'] == -1) {
+        for (char c : tArr) {
+            if (dictIndex[0][c - 'a'] == -1) return -1;
+            if (dictIndex[idx][c - 'a'] == -1) {
                 ++ans;
                 idx = 0;
             }
 
             idx = dictIndex[idx][c - 'a'] + 1;
-            if(idx == M) {
+            if (idx == S) {
                 ++ans;
                 idx = 0;
             }
