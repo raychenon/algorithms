4ec00c32b3ff1a2ce55dda9155a51a3a1a3cf8e6
Raymond Chenon <raymond.chenon@gmail.com>
2019-10-14 11:14:24 +0200

    #105  brute force solution

--- src/main/java/com/raychenon/leetcode/ShortestWaytoFormString.java
+++ src/main/java/com/raychenon/leetcode/ShortestWaytoFormString.java
@@ -7,6 +7,17 @@ package com.raychenon.leetcode;
  */
 public class ShortestWaytoFormString {
     public int shortestWay(String source, String target) {
-
+        char[] cs = source.toCharArray(), ts = target.toCharArray();
+        int res = 0;
+        for (int i = 0; i < ts.length; ) {
+            int oriI = i;
+            for (int j = 0; j < cs.length; j++) {
+                if (i < ts.length && cs[j] == ts[i])
+                    i++;
+            }
+            if (i == oriI) return -1;
+            res++;
+        }
+        return res;
     }
 }
--- src/main/kotlin/com/raychenon/kotlin/leetcode/ShortestWaytoFormString.kt
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/ShortestWaytoFormString.kt
@@ -1,12 +1,33 @@
 package com.raychenon.kotlin.leetcode
 
+
 /**
  * User: raychenon
  * Date: 2019-10-13
  * https://leetcode.com/problems/shortest-way-to-form-string/
  */
 object ShortestWaytoFormString {
+
+    /**
+     * time complexity: O(S*T) , where S: length of source and T: length of target
+     */
     fun shortestWay(source: String, target: String): Int {
+
+        val sc = source.toCharArray()
+        val ts = target.toCharArray()
+        var res = 0
+        var i = 0
+        while (i < ts.size) {
+            val indexI = i
+            for (j in sc.indices) {
+                if (i < ts.size && sc[j] == ts[i])
+                    i++
+            }
+            // if target contains at least a letter that "source" doesn't have, then return -1
+            if (i == indexI) return -1
+            res++
+        }
+        return res
 
     }
 }
