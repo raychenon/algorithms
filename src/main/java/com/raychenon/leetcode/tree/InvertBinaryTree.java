656bc6a0b5cd6343efad0a8266f28748635171b9
Raymond Chenon <raymond.chenon@gmail.com>
2019-10-21 23:39:28 +0200

    #101 invert binary tree

---
+++ src/main/java/com/raychenon/leetcode/tree/InvertBinaryTree.java
@@ -0,0 +1,12 @@
+package com.raychenon.leetcode.tree;
+
+/**
+ * User: raychenon
+ * Date: 2019-10-21
+ * https://leetcode.com/problems/invert-binary-tree/
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+
+    }
+}
---
+++ src/main/java/com/raychenon/leetcode/tree/TreeNode.java
@@ -0,0 +1,17 @@
+package com.raychenon.leetcode.tree;
+
+/**
+ * User: raychenon
+ * Date: 2019-10-21
+ * https://leetcode.com/problems/invert-binary-tree/
+ */
+public class TreeNode {
+    int val;
+    com.raychenon.leetcode.TreeNode left;
+    com.raychenon.leetcode.TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
