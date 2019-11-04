112b1f0ea37662d18d7fbd6ba6e601d2983b3d4c
Raymond Chenon <raymond.chenon@gmail.com>
2019-11-04 21:50:26 +0100

    #124 partial solution

--- src/main/java/com/raychenon/leetcode/GenerateParenthesis.java
+++ src/main/java/com/raychenon/leetcode/GenerateParenthesis.java
@@ -1,5 +1,7 @@
 package com.raychenon.leetcode;
 
+
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -10,7 +12,19 @@ import java.util.List;
 public class GenerateParenthesis {
 
     public List<String> generateParenthesis(int n) {
+        List<String> ans = new LinkedList<>();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
 
+    private void backtrack(List<String> ans, String cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+
+        if (open < max) backtrack(ans, cur + "(", open + 1, close, max);
+        if (close < open) backtrack(ans, cur + ")", open, close + 1, max);
     }
 
 }
