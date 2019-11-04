2e422eb3ca875fa78f2f23f9c9a768ab192e68a6
Raymond Chenon <raymond.chenon@gmail.com>
2019-11-04 21:43:01 +0100

    #124 generate parenthesis

---
+++ src/main/java/com/raychenon/leetcode/GenerateParenthesis.java
@@ -0,0 +1,16 @@
+package com.raychenon.leetcode;
+
+import java.util.List;
+
+/**
+ * User: raychenon
+ * Date: 2019-11-04
+ * https://leetcode.com/problems/generate-parentheses/
+ */
+public class GenerateParenthesis {
+
+    public List<String> generateParenthesis(int n) {
+
+    }
+
+}
