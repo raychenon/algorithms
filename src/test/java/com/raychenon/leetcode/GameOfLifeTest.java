8449bd5f864dab0e0b41755115a8626ab80cfa4f
Raymond Chenon <raymond.chenon@gmail.com>
2019-11-09 22:57:29 +0100

    #94 add test

--- src/test/java/com/raychenon/leetcode/GameOfLifeTest.java
+++ src/test/java/com/raychenon/leetcode/GameOfLifeTest.java
@@ -1,9 +1,43 @@
 package com.raychenon.leetcode;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static junit.framework.TestCase.assertTrue;
+
 /**
  * User: raychenon
  * Date: 2019-11-07
  * https://leetcode.com/problems/game-of-life/
  */
 public class GameOfLifeTest {
+
+
+    private static GameOfLife gameOfLife;
+
+    @BeforeClass
+    public static void setUp() {
+        gameOfLife = new GameOfLife();
+    }
+
+    @Test
+    public void gameOfLifeTest() {
+        int[][] grid = new int[][]{
+                {0, 1, 0},
+                {0, 0, 1},
+                {1, 1, 1},
+                {0, 0, 0}
+        };
+        int[][] expected = new int[][]{
+                {0, 0, 0},
+                {1, 0, 1},
+                {0, 1, 1},
+                {0, 1, 0}
+        };
+
+        gameOfLife.gameOfLife(grid);
+        assertTrue(Arrays.equals(expected, grid));
+    }
 }
