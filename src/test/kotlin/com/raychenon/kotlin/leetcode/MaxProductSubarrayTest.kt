c232db424bfd1e1c0956ce4ed153f4a332d60c0d
Raymond Chenon <raymond.chenon@gmail.com>
2019-11-11 20:28:11 +0100

    Test with Float

--- src/test/kotlin/com/raychenon/kotlin/leetcode/MaxProductSubarrayTest.kt
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/MaxProductSubarrayTest.kt
@@ -13,34 +13,46 @@ class MaxProductSubarrayTest {
     @Test
     fun example1Test() {
         val nums = floatArrayOf(2f, 3f, -2f, 4f)
-        assertMaxProduct(6f,nums)
+        assertMaxProduct(6f, nums)
     }
 
     @Test
     fun example2Test() {
-        val nums = floatArrayOf(2f, 3f, 3f,-2f, 4f)
-        assertMaxProduct(18f,nums)
+        val nums = floatArrayOf(2f, 3f, 3f, -2f, 4f)
+        assertMaxProduct(18f, nums)
     }
 
     @Test
     fun productSize2Test() {
         val nums = floatArrayOf(0f, 2f)
-        assertMaxProduct(2f,nums)
+        assertMaxProduct(2f, nums)
     }
 
     @Test
     fun productSize3Test() {
         val nums = floatArrayOf(-2f, 0.1f, -1f)
-        assertMaxProduct(0.2f,nums)
+        assertMaxProduct(0.2f, nums)
+    }
+
+    @Test
+    fun onlyPositiveFloatTest() {
+        val nums = floatArrayOf(0.1f, 0.9f, 0.5f)
+        assertMaxProduct(0.9f, nums)
+    }
+
+    @Test
+    fun intNFloatTest() {
+        val nums = floatArrayOf(-50f, -0.1f, 0.9f, -0.8f, 0.5f)
+        assertMaxProduct(5f, nums)
     }
 
     @Test
     fun emptyArray3Test() {
         val nums = floatArrayOf()
-        assertMaxProduct(0f,nums)
+        assertMaxProduct(0f, nums)
     }
 
-    fun assertMaxProduct(expected: Float, nums: FloatArray){
+    fun assertMaxProduct(expected: Float, nums: FloatArray) {
         assertEquals(expected, MaxProductSubarray.maxProductBrute1(nums))
         assertEquals(expected, MaxProductSubarray.maxProductBrute2(nums))
 
