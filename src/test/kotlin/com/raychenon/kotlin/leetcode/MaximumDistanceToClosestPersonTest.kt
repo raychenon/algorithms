8fb670567099b2536ad6db2280a0dcc34921a798
Raymond Chenon <raymond.chenon@gmail.com>
2019-11-23 12:03:28 +0100

    #134 comment graphically

--- src/test/kotlin/com/raychenon/kotlin/leetcode/MaximumDistanceToClosestPersonTest.kt
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/MaximumDistanceToClosestPersonTest.kt
@@ -13,39 +13,37 @@ class MaximumDistanceToClosestPersonTest {
     @Test
     fun example1Test() {
         val input = intArrayOf(1, 0, 0, 0, 1, 0, 1)
-        //             <-----|----->
+        //                     <-----|----->
         evaluateMaximumDistanceToClosestPerson(2, input)
     }
 
     @Test
     fun noOneonRightTest() {
         val input = intArrayOf(1, 0, 0, 0)
-        //             |-------->
+        //                     |-------->
         evaluateMaximumDistanceToClosestPerson(3, input)
     }
-
 
     @Test
     fun noOneonLeftTest() {
         val input = intArrayOf(0, 0, 0, 1)
-        //             <--------|
+        //                     <--------|
         evaluateMaximumDistanceToClosestPerson(3, input)
     }
 
     @Test
     fun middleEvenTest() {
         val input = intArrayOf(1, 0, 0, 1)
-        //             |--->
+        //                     |--->
         evaluateMaximumDistanceToClosestPerson(1, input)
     }
 
     @Test
     fun middleOddTest() {
         val input = intArrayOf(1, 0, 0, 0, 1)
-        //             |-----><----|
+        //                     |-----><----|
         evaluateMaximumDistanceToClosestPerson(2, input)
     }
-
 
     fun evaluateMaximumDistanceToClosestPerson(expected: Int, seats: IntArray) {
         Assert.assertEquals(expected, MaximumDistanceToClosestPerson.maxDistToClosest(seats))
