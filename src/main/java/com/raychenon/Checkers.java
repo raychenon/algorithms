3244529a020f85eff7a6a41646036de8fc35d368
Raymond Chenon <raymond.chenon@gmail.com>
2019-11-24 21:26:30 +0100

    start code Checkers with TDD

---
+++ src/main/java/com/raychenon/Checkers.java
@@ -0,0 +1,12 @@
+package com.raychenon;
+
+/**
+ * User: raychenon
+ * Date: 2019-11-24
+ */
+public class Checkers {
+
+    public int countPawnsBeaten(String[] board){
+        return 2;
+    }
+}
---
+++ src/test/java/CheckersTest.java
@@ -0,0 +1,47 @@
+import com.raychenon.Checkers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * User: raychenon
+ * Date: 2019-11-24
+ */
+public class CheckersTest {
+
+    private static Checkers instance;
+
+    @BeforeClass
+    public static void setUp() {
+        instance = new Checkers();
+    }
+
+    @Test
+    public void board1Test() {
+
+        String[] board = new String[]{
+                "..X...",
+                "......",
+                "....X.",
+                ".X....",
+                "..X.X.",
+                "...O.."
+        };
+        assertEquals(2, instance.countPawnsBeaten(board));
+    }
+
+    @Test
+    public void board2Test() {
+
+        String[] board = new String[]{
+                "X....",
+                ".X...",
+                "..O..",
+                "...X.",
+                "....."
+        };
+        assertEquals(0, instance.countPawnsBeaten(board));
+    }
+
+}
