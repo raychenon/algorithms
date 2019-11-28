b7aaac1f310747ebb0a156c9d6f0a92249df6245
Raymond Chenon <raymond.chenon@gmail.com>
2019-11-29 00:40:22 +0100

    #94 test

--- src/test/kotlin/com/raychenon/kotlin/leetcode/GameOfLifeTest.kt
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/GameOfLifeTest.kt
@@ -1,9 +1,31 @@
 package com.raychenon.kotlin.leetcode
 
+import org.junit.Assert
+import org.junit.Test
+
 /**
  * User: raychenon
  * Date: 2019-11-07
  * https://leetcode.com/problems/game-of-life/
  */
 class GameOfLifeTest {
+
+    @Test
+    fun gameofLifeTest() {
+        val board = arrayOf(
+            intArrayOf(0, 1, 0),
+            intArrayOf(0, 0, 1),
+            intArrayOf(1, 1, 1),
+            intArrayOf(0, 0, 0)
+        )
+        val result = arrayOf(
+            intArrayOf(0, 0, 0),
+            intArrayOf(1, 0, 1),
+            intArrayOf(0, 1, 1),
+            intArrayOf(0, 1, 0)
+        )
+
+        GameOfLife.gameOfLife(board)
+        Assert.assertEquals(result, board)
+    }
 }
