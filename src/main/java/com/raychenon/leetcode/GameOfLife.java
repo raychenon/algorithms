a79d68bdd5605927a7810155f4f9a14c0568bbad
Raymond Chenon <raymond.chenon@gmail.com>
2019-11-29 09:57:21 +0100

    #94 copy the solution

--- src/main/java/com/raychenon/leetcode/GameOfLife.java
+++ src/main/java/com/raychenon/leetcode/GameOfLife.java
@@ -7,7 +7,62 @@ package com.raychenon.leetcode;
  */
 public class GameOfLife {
 
+    /**
+     * time complexity : O(R*C) R number of rows, C number of columns
+     * space complexity : O(R*C) R number of rows, C number of columns
+     * @param board
+     */
     public void gameOfLife(int[][] board) {
+        // Neighbors array to find 8 neighboring cells for a given cell
+        int[] neighbors = {0, 1, -1};
 
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // Create a copy of the original board
+        int[][] copyBoard = new int[rows][cols];
+
+        // Create a copy of the original board
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                copyBoard[row][col] = board[row][col];
+            }
+        }
+
+        // Iterate through board cell by cell.
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                // For each cell count the number of live neighbors.
+                int liveNeighbors = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+
+                        if (!(neighbors[i] == 0 && neighbors[j] == 0)) {
+                            int r = (row + neighbors[i]);
+                            int c = (col + neighbors[j]);
+
+                            // Check the validity of the neighboring cell.
+                            // and whether it was originally a live cell.
+                            // The evaluation is done against the copy, since that is never updated.
+                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (copyBoard[r][c] == 1)) {
+                                liveNeighbors += 1;
+                            }
+                        }
+                    }
+                }
+
+                // Rule 1: Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+                // Rule 3: Any live cell with more than three live neighbors dies, as if by over-population..
+                if ((copyBoard[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[row][col] = 0;
+                }
+                // Rule 4: Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+                if (copyBoard[row][col] == 0 && liveNeighbors == 3) {
+                    board[row][col] = 1;
+                }
+            }
+        }
     }
 }
