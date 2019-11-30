6367cdc75566ed478f4bb7e103c53d0324cf339e
Raymond Chenon <raymond.chenon@gmail.com>
2019-12-01 00:47:03 +0100

    #128 implementation in Kotlin

---
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/QueensThatCanAttackTheKing.kt
@@ -0,0 +1,50 @@
+package com.raychenon.kotlin.leetcode
+
+/**
+ * User: raychenon
+ * Date: 2019-12-01
+ * https://leetcode.com/problems/queens-that-can-attack-the-king/
+ */
+object QueensThatCanAttackTheKing {
+
+    fun queensAttacktheKing(queens: Array<IntArray>, king: IntArray): List<List<Int>> {
+
+        // From the King position, see who can attack horizontally,vertically,diagonally
+        // any Queens on these coordinates is on the answer list.
+        val dir = arrayOf(
+            intArrayOf(0, 1),
+            intArrayOf(0, -1),
+            intArrayOf(1, 0),
+            intArrayOf(-1, 0),
+            intArrayOf(1, 1),
+            intArrayOf(1, -1),
+            intArrayOf(-1, 1),
+            intArrayOf(-1, -1)
+        )
+        var isVisitedArr = BooleanArray(dir.size)
+        val table = Array(8) { BooleanArray(8) }
+
+        // copy the queens positions in the 8x8 chessboard
+        for (i in queens.indices) {
+            table[queens[i][0]][queens[i][1]] = true
+        }
+        val list = mutableListOf<List<Int>>()
+
+        for (count in 0 until 8) {
+            for (p in 0 until dir.size) {
+
+                val row = king[0] + dir[p][0] * count
+                val col = king[1] + dir[p][1] * count
+                if (col >= 0 && col < 8 && row >= 0 && row < 8 && table[row][col] && !isVisitedArr[count]) {
+                    isVisitedArr[p] = true
+
+                    val validQueen = intArrayOf(row, col).toList()
+                    list.add(validQueen)
+                }
+            }
+        }
+
+        return list
+    }
+
+}
---
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/QueensThatCanAttackTheKingTest.kt
@@ -0,0 +1,132 @@
+package com.raychenon.kotlin.leetcode
+
+import junit.framework.TestCase
+import org.junit.Test
+import java.util.*
+
+/**
+ * User: raychenon
+ * Date: 2019-12-01
+ * https://leetcode.com/problems/queens-that-can-attack-the-king/
+ */
+class QueensThatCanAttackTheKingTest {
+
+
+    // |K|A| | |Q| | | |
+    // |A| | | | | | | |
+    // | | | | |Q| | | |
+    // | | | |A| | | | |
+    // |Q| | | | | | | |
+    // | | | | | | | | |
+    // | | | | | | | | |
+    // | | | | | | | | |
+    @Test
+    fun queens1Test() {
+        val queens = arrayOf(
+            intArrayOf(0, 1),
+            intArrayOf(1, 0),
+            intArrayOf(4, 0),
+            intArrayOf(0, 4),
+            intArrayOf(3, 3),
+            intArrayOf(2, 4)
+        )
+        val king = intArrayOf(0, 0)
+        val expected = arrayOf(intArrayOf(0, 1), intArrayOf(1, 0), intArrayOf(3, 3))
+        compareMethods(expected, queens, king)
+    }
+
+
+    @Test
+    fun queens2Test() {
+        val queens = arrayOf(
+            intArrayOf(0, 0),
+            intArrayOf(1, 1),
+            intArrayOf(2, 2),
+            intArrayOf(3, 4),
+            intArrayOf(3, 5),
+            intArrayOf(4, 4),
+            intArrayOf(4, 5)
+        )
+        val king = intArrayOf(3, 3)
+        val expected = arrayOf(intArrayOf(2, 2), intArrayOf(3, 4), intArrayOf(4, 4))
+        compareMethods(expected, queens, king)
+    }
+
+
+    @Test
+    fun queens3Test() {
+        val queens = arrayOf(
+            intArrayOf(5, 6),
+            intArrayOf(7, 7),
+            intArrayOf(2, 1),
+            intArrayOf(0, 7),
+            intArrayOf(1, 6),
+            intArrayOf(5, 1),
+            intArrayOf(3, 7),
+            intArrayOf(0, 3),
+            intArrayOf(4, 0),
+            intArrayOf(1, 2),
+            intArrayOf(6, 3),
+            intArrayOf(5, 0),
+            intArrayOf(0, 4),
+            intArrayOf(2, 2),
+            intArrayOf(1, 1),
+            intArrayOf(6, 4),
+            intArrayOf(5, 4),
+            intArrayOf(0, 0),
+            intArrayOf(2, 6),
+            intArrayOf(4, 5),
+            intArrayOf(5, 2),
+            intArrayOf(1, 4),
+            intArrayOf(7, 5),
+            intArrayOf(2, 3),
+            intArrayOf(0, 5),
+            intArrayOf(4, 2),
+            intArrayOf(1, 0),
+            intArrayOf(2, 7),
+            intArrayOf(0, 1),
+            intArrayOf(4, 6),
+            intArrayOf(6, 1),
+            intArrayOf(0, 6),
+            intArrayOf(4, 3),
+            intArrayOf(1, 7)
+        )
+        val king = intArrayOf(3, 4)
+        val expected = arrayOf(
+            intArrayOf(2, 3),
+            intArrayOf(1, 4),
+            intArrayOf(1, 6),
+            intArrayOf(3, 7),
+            intArrayOf(4, 3),
+            intArrayOf(5, 4),
+            intArrayOf(4, 5)
+        )
+        compareMethods(expected, queens, king)
+    }
+
+
+    fun compareMethods(
+        expectedQueens: Array<IntArray>,
+        queens: Array<IntArray>,
+        king: IntArray
+    ) {
+        val expectedQueensSubset: MutableSet<QueenCoordinate> = HashSet()
+        for (queen in expectedQueens) {
+            val coord = QueenCoordinate(queen[0], queen[1])
+            expectedQueensSubset.add(coord)
+        }
+        val actualQueensSubset: MutableSet<QueenCoordinate> = HashSet()
+        for (queenCoordinates in QueensThatCanAttackTheKing.queensAttacktheKing(
+            queens,
+            king
+        )) {
+            val temp = QueenCoordinate(queenCoordinates[0], queenCoordinates[1])
+            actualQueensSubset.add(temp)
+        }
+        TestCase.assertTrue(actualQueensSubset.containsAll(expectedQueensSubset))
+        TestCase.assertTrue(expectedQueensSubset == actualQueensSubset)
+    }
+
+
+    data class QueenCoordinate(val row: Int, val col: Int)
+}
