912567055489bb0e73326d64ca9708210366fa3c
Raymond Chenon <raymond.chenon@gmail.com>
2019-11-30 16:11:44 +0100

    #128 include all vertical, horizontal and diagonal . Fix must only take the first in line of sight.

--- src/main/java/com/raychenon/leetcode/QueensThatCanAttackTheKing.java
+++ src/main/java/com/raychenon/leetcode/QueensThatCanAttackTheKing.java
@@ -3,6 +3,7 @@ package com.raychenon.leetcode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * User: raychenon
@@ -18,12 +19,81 @@ import java.util.List;
 public class QueensThatCanAttackTheKing {
 
     public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
+        List<List<Integer>> ans = new ArrayList<>();
 
-        List<List<Integer>> list = new ArrayList<>();
-        list.add(Arrays.asList(0, 1));
-        list.add(Arrays.asList(1, 0));
-        list.add(Arrays.asList(3, 3));
-        return list;
+        // From the King perspectice, who can attack horizontally,vertically,diagonally
+        // any Queens on these coordinates is on the answer list.
+
+        // vertical
+        for (int[] queen : queens) {
+            if (king[0] == queen[0]) {
+                List<Integer> list = Arrays.stream(queen).boxed().collect(Collectors.toList());
+                ans.add(list);
+            }
+        }
+
+        // horizontal
+        for (int[] queen : queens) {
+            if (king[1] == queen[1]) {
+                List<Integer> list = Arrays.stream(queen).boxed().collect(Collectors.toList());
+                ans.add(list);
+            }
+        }
+
+
+        // diagonal  \
+        for (int[] queen : queens) {
+            int row = 0;
+            int col = 0;
+            while (king[0] + row < 8 && king[0] + row >= 0 && king[1] + col < 8 && king[1] + col >= 0) {
+                row++;
+                col++;
+                if (queen[0] == king[0] + row && queen[1] == king[1] + col) {
+                    List<Integer> list = Arrays.stream(queen).boxed().collect(Collectors.toList());
+                    ans.add(list);
+                }
+            }
+
+            row = 0;
+            col = 0;
+            while (king[0] + row < 8 && king[0] + row >= 0 && king[1] + col < 8 && king[1] + col >= 0) {
+                --row;
+                --col;
+                if (queen[0] == king[0] + row && queen[1] == king[1] + col) {
+                    List<Integer> list = Arrays.stream(queen).boxed().collect(Collectors.toList());
+                    ans.add(list);
+                }
+            }
+        }
+
+
+        // diagonal  /
+        for (int[] queen : queens) {
+            int row = 0;
+            int col = 0;
+            while (king[0] + row < 8 && king[0] + row >= 0 && king[1] + col < 8 && king[1] + col >= 0) {
+                row--;
+                col++;
+                if (queen[0] == king[0] + row && queen[1] == king[1] + col) {
+                    List<Integer> list = Arrays.stream(queen).boxed().collect(Collectors.toList());
+                    ans.add(list);
+                }
+            }
+
+            row = 0;
+            col = 0;
+            while (king[0] + row < 8 && king[0] + row >= 0 && king[1] + col < 8 && king[1] + col >= 0) {
+                ++row;
+                --col;
+                if (queen[0] == king[0] + row && queen[1] == king[1] + col) {
+                    List<Integer> list = Arrays.stream(queen).boxed().collect(Collectors.toList());
+                    ans.add(list);
+                }
+            }
+        }
+
+        return ans;
     }
+
 
 }
