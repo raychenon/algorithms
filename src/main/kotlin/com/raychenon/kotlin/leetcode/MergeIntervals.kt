69ce11221a6ca4f649061ae51a97e7379836b732
Raymond Chenon <raymond.chenon@gmail.com>
2020-01-30 23:19:17 +0100

    temp solution missing the last element

--- src/main/kotlin/com/raychenon/kotlin/leetcode/MergeIntervals.kt
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/MergeIntervals.kt
@@ -8,6 +8,18 @@ package com.raychenon.kotlin.leetcode
 object MergeIntervals {
 
     fun merge(intervals: Array<IntArray>): Array<IntArray> {
+        var arr = mutableListOf<IntArray>()
+        var prevSubArr = intervals.get(0)
+        for (i in 1 until intervals.size) {
+            var current = intervals.get(i)
+            if (prevSubArr[1] >= current[0]) {
+                prevSubArr[1] = current.get(1)
+            } else {
+                arr.add(prevSubArr)
+                prevSubArr = current
+            }
+        }
 
+        return arr.toTypedArray()
     }
 }
