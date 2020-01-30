7ee7591948570d25b3c560d9654b196a533188b8
Raymond Chenon <raymond.chenon@gmail.com>
2020-01-30 23:06:53 +0100

    #145 Merge intervals

---
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/MergeIntervals.kt
@@ -0,0 +1,13 @@
+package com.raychenon.kotlin.leetcode
+
+/**
+ * User: raychenon
+ * Date: 2020-01-30
+ * https://leetcode.com/problems/merge-intervals/
+ */
+object MergeIntervals {
+
+    fun merge(intervals: Array<IntArray>): Array<IntArray> {
+
+    }
+}
---
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/MergeIntervalsTest.kt
@@ -0,0 +1,31 @@
+package com.raychenon.kotlin.leetcode
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * User: raychenon
+ * Date: 2020-01-30
+ * https://leetcode.com/problems/merge-intervals/
+ */
+class MergeIntervalsTest {
+
+
+    @Test
+    fun example1Test() {
+        val input = arrayOf(intArrayOf(1, 3), intArrayOf(2, 6), intArrayOf(8, 10), intArrayOf(15, 18))
+        Assert.assertEquals(
+            arrayOf(intArrayOf(1, 6), intArrayOf(8, 10), intArrayOf(15, 18)),
+            MergeIntervals.merge(input)
+        )
+    }
+
+    @Test
+    fun example2Test() {
+        val input = arrayOf(intArrayOf(1, 4), intArrayOf(4, 5))
+        Assert.assertEquals(
+            arrayOf(intArrayOf(1, 5)),
+            MergeIntervals.merge(input)
+        )
+    }
+}
