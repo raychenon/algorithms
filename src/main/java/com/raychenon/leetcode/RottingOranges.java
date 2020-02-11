5b874eac4c79c3c336a0e0d6967530ee3934b22c
Raymond Chenon <raymond.chenon@gmail.com>
2020-02-11 23:56:56 +0100

    #150 start rotting oranges

---
+++ src/main/java/com/raychenon/leetcode/RottingOranges.java
@@ -0,0 +1,17 @@
+package com.raychenon.leetcode;
+
+/**
+ * User: raychenon
+ * Date: 11/2/2020
+ * https://leetcode.com/problems/rotting-oranges/
+ */
+public class RottingOranges {
+
+    public static final int EMPTY = 0;
+    public static final int FRESH = 1;
+    public static final int ROTTEN = 2;
+
+    public int orangesRotting(int[][] grid) {
+
+    }
+}
