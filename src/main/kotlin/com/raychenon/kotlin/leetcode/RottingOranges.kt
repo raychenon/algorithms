280ae1a56ce7cec066b84719cd49ad5048f95f37
Raymond Chenon <raymond.chenon@gmail.com>
2020-02-16 21:25:36 +0100

    #150 test rotting oranges in Kotlin

---
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/RottingOranges.kt
@@ -0,0 +1,13 @@
+package com.raychenon.kotlin.leetcode
+
+/**
+ * User: raychenon
+ * Date: 16/2/2020
+ * https://leetcode.com/problems/rotting-oranges/
+ */
+object RottingOranges {
+
+    fun orangesRotting(grid: Array<IntArray>): Int {
+
+    }
+}
---
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/RottingOrangesTest.kt
@@ -0,0 +1,33 @@
+package com.raychenon.kotlin.leetcode
+
+import junit.framework.TestCase
+import org.junit.Test
+
+/**
+ * User: raychenon
+ * Date: 16/2/2020
+ * https://leetcode.com/problems/rotting-oranges/
+ */
+class RottingOrangesTest {
+
+    @Test
+    fun example1Test() {
+        val matrix =
+            arrayOf(intArrayOf(2, 1, 1), intArrayOf(1, 1, 0), intArrayOf(0, 1, 1))
+        TestCase.assertEquals(4, RottingOranges.orangesRotting(matrix))
+    }
+
+    @Test
+    fun example2Test() {
+        val matrix =
+            arrayOf(intArrayOf(2, 1, 1), intArrayOf(0, 1, 1), intArrayOf(1, 0, 1))
+        TestCase.assertEquals(-1, RottingOranges.orangesRotting(matrix))
+    }
+
+    @Test
+    fun example3Test() {
+        val matrix = arrayOf(intArrayOf(0, 2))
+        TestCase.assertEquals(0, RottingOranges.orangesRotting(matrix))
+    }
+    
+}
