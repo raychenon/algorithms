8a90ce206382d32a699351f4f4aada9488632599
Raymond Chenon <raymond.chenon@gmail.com>
2020-02-28 23:51:25 +0100

    String compression in Python

---
+++ python3/algorithms/leetcode/string_compression.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+"""
+https://leetcode.com/problems/string-compression/
+"""
+def compress(self, chars: List[str]) -> int:
+
+
+if __name__ == "__main__":
+	assert compress(["a","a","b","b","c","c","c"]) == 6
+	assert compress(["a"]) == 1
