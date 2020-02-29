22d333ee989a0ab905dae6ffcaef3799fbb5df1a
Raymond Chenon <raymond.chenon@gmail.com>
2020-02-29 02:02:09 +0100

    solution in python 3

--- python3/algorithms/leetcode/string_compression.py
+++ python3/algorithms/leetcode/string_compression.py
@@ -3,8 +3,19 @@
 """
 https://leetcode.com/problems/string-compression/
 """
-def compress(self, chars: List[str]) -> int:
-
+def compress(chars: List[str]) -> int:
+    left = i = 0
+    while i < len(chars):
+        char, length = chars[i],1
+        while(i+1) < len(chars) and char == chars[i+1]:
+            length,i = length + 1, i + 1
+        chars[left] = char
+        if length > 1:
+            len_str = str(length)
+            chars[left+1: left+1 + len(len_str)] = len_str
+            left += len(len_str)
+        left,i = left+1, i+1
+    return left
 
 if __name__ == "__main__":
 	assert compress(["a","a","b","b","c","c","c"]) == 6
