c91d831f819ac0ea31a63c185e74b0ba098235e3
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-03 23:55:21 +0100

    #155 write more efficient algo

--- python3/algorithms/leetcode/add_binary.py
+++ python3/algorithms/leetcode/add_binary.py
@@ -1,7 +1,16 @@
 #!/bin/python3
 
+"""
+time complexity: O(A+B)
+space complexity: O(max(A+B))
+"""
 def addBinary(a: str,b: str) -> str:
-	return '{0:b}'.format(int(a,2) + int(b,2))
+	x, y = int(a,2), int(b,2)
+	while y:
+		answer = x ^ y
+		carry = (x & y) << 1
+		x, y = answer, carry
+	return bin(x)[2:]
 
 if __name__ == "__main__":
 	assert addBinary("11","1") == "100"
