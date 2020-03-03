9e078446f018b5582055c8c3d33f3bbf0036585c
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-03 23:15:44 +0100

    #155 add binary in python

---
+++ python3/algorithms/leetcode/add_binary.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+def addBinary(a,b) -> str:
+	return '{0:b}'.format(int(a,2) + int(b,2))
+
+if __name__ == "__main__":
+	assert addBinary("11","1") == "100"
+	assert addBinary("1010","1011") == "10101"
