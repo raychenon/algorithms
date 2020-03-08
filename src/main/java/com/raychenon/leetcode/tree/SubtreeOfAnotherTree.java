8cbe03027229f71b96d0df5e162245479a6e079a
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-08 16:15:45 +0100

    #157 subtree of another tree

---
+++ src/main/java/com/raychenon/leetcode/tree/SubtreeOfAnotherTree.java
@@ -0,0 +1,16 @@
+package com.raychenon.leetcode.tree;
+
+import com.raychenon.leetcode.tree.TreeNode;
+
+/**
+ * User: raychenon
+ * Date: 8/3/2020
+ * https://leetcode.com/problems/subtree-of-another-tree
+ */
+public class SubtreeOfAnotherTree {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return true;
+    }
+
+}
---
+++ src/test/java/com/raychenon/leetcode/tree/SubtreeOfAnotherTreeTest.java
@@ -0,0 +1,9 @@
+package com.raychenon.leetcode.tree;
+
+/**
+ * User: raychenon
+ * Date: 8/3/2020
+ * https://leetcode.com/problems/subtree-of-another-tree
+ */
+public class SubtreeOfAnotherTreeTest {
+}
