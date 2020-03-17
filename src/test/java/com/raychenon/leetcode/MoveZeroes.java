8145dd2f750028cf12ec8986395cc87702b113d1
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-17 23:51:31 +0100

    #159 start move zeros exercise

---
+++ src/test/java/com/raychenon/leetcode/MoveZeroes.java
@@ -0,0 +1,13 @@
+package com.raychenon.leetcode;
+
+/**
+ * User: raychenon
+ * Date: 17/3/2020
+ * https://leetcode.com/problems/move-zeroes/
+ */
+public class MoveZeroes {
+
+    public void moveZeroes(int[] nums) {
+
+    }
+}
---
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/MoveZeroes.kt
@@ -0,0 +1,13 @@
+package com.raychenon.kotlin.leetcode
+
+/**
+ * User: raychenon
+ * Date: 17/3/2020
+ * https://leetcode.com/problems/move-zeroes/
+ */
+object MoveZeroes {
+
+    fun moveZeroes(nums: IntArray): Unit {
+
+    }
+}
