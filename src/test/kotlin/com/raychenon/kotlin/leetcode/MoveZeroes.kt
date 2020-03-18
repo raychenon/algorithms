8e97ad4d85104ea63e4848f4dab97219923914d7
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-19 00:48:55 +0100

    initial idea

--- src/test/kotlin/com/raychenon/kotlin/leetcode/MoveZeroes.kt
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/MoveZeroes.kt
@@ -8,6 +8,25 @@ package com.raychenon.kotlin.leetcode
 object MoveZeroes {
 
     fun moveZeroes(nums: IntArray): Unit {
+        var count = nums.toList().filter { x -> x == 0 }.count()
 
+        var temp = 0
+        for (i in nums.size - 1..0 step -1) {
+            // put only 0 and shift
+            if(i >= nums.size - count){
+                if (nums.get(i) == 0) {
+                    nums[i] = 0
+                }else{
+                    nums[i] = temp
+                    temp = nums.get(i-1)
+                }
+            }else{
+
+            }
+
+            val value = nums.get(i)
+            if (nums.get(i) != 0) {
+            }
+        }
     }
 }
