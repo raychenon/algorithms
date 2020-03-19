a66ed85e96b9cb9c6ea7e02a3f0dacd3e54db3e8
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-20 00:36:14 +0100

    #159refactor the code in Python like i n Java

--- python3/algorithms/leetcode/move_zeroes.py
+++ python3/algorithms/leetcode/move_zeroes.py
@@ -7,11 +7,14 @@ import re
 https://leetcode.com/problems/move-zeroes/
 """
 def moveZeroes(nums: List[int]) -> None:
-    last_non_zeroes = 0
+    acc_zeroes_count = 0
     for i in range(len(nums)):
-        if nums[i] != 0:
-            nums[i], nums[last_non_zeroes] = nums[last_non_zeroes], nums[i]
-            last_non_zeroes += 1
+        if nums[i] == 0:
+            acc_zeroes_count += 1
+        elif acc_zeroes_count > 0:
+            temp = nums[i]
+            nums[i] = 0
+            nums[i-acc_zeroes_count] = temp
 
 if __name__ == "__main__":
     input = [0,1,0,3,12]
