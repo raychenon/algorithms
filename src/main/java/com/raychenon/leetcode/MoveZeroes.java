29f380cbb295212d96bcca0e1f4116065e6a77ee
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-20 00:16:08 +0100

    #159 more performant code in Java

--- src/main/java/com/raychenon/leetcode/MoveZeroes.java
+++ src/main/java/com/raychenon/leetcode/MoveZeroes.java
@@ -11,18 +11,20 @@ public class MoveZeroes {
      * time complexity: O(n)
      * space complexity: O(1)
      *
+     * solution https://leetcode.com/problems/move-zeroes/discuss/172432/THE-EASIEST-but-UNUSUAL-snowball-JAVA-solution-BEATS-100-(O(n))-%2B-clear-explanation
      * @param nums
      */
     public void moveZeroes(int[] nums) {
-        int lastNonZeroFoundAt = 0;
+        int accumulatedZeroSize = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[lastNonZeroFoundAt++] = nums[i];
+            if (nums[i] == 0) {
+                accumulatedZeroSize++;
+            } else if (accumulatedZeroSize > 0) {
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[i - accumulatedZeroSize] = temp;
             }
         }
 
-        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
-            nums[i] = 0;
-        }
     }
 }
