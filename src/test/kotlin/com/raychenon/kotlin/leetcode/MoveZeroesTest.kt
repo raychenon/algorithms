0c2332b070f21b51daf8a8b266579bcb420b362e
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-19 01:21:05 +0100

    re work

--- src/test/kotlin/com/raychenon/kotlin/leetcode/MoveZeroesTest.kt
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/MoveZeroesTest.kt
@@ -14,7 +14,18 @@ class MoveZeroesTest {
     fun moveZeroesTest() {
         var input = intArrayOf(0, 1, 0, 3, 12)
         MoveZeroes.moveZeroes(input)
-        Assert.assertEquals(intArrayOf(1, 3, 12, 0, 0), input)
+
+        Assert.assertArrayEquals(intArrayOf(1, 3, 12, 0, 0), input)
     }
+
+
+    @Test
+    fun moveZeroesTest2() {
+        var input = intArrayOf(0, 1, 0, 3, 5, 5, 0, 11, 12)
+        MoveZeroes.moveZeroes(input)
+
+        Assert.assertArrayEquals(intArrayOf(1, 3, 5, 5, 11, 12, 0, 0, 0), input)
+    }
+
 
 }
