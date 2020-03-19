0735e3168915f56fe003ef4fdfd1bf60f7783009
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-19 23:53:25 +0100

    #159 test in python

--- python3/algorithms/leetcode/move_zeroes.py
+++ python3/algorithms/leetcode/move_zeroes.py
@@ -7,3 +7,13 @@ import re
 https://leetcode.com/problems/move-zeroes/
 """
 def moveZeroes(nums: List[int]) -> None:
+    last_non_zeroes = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[i], nums[last_non_zeroes] = nums[last_non_zeroes], nums[i]
+            last_non_zeroes += 1
+
+if __name__ == "__main__":
+    input = [0,1,0,3,12]
+    moveZeroes(input)
+    assert input == [1,3,12,0,0]
