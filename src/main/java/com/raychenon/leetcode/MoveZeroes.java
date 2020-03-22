fad20078f3ba66e8d0f62b2e1f60096d515d570f
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-22 13:53:48 +0100

    #159 explanation of snow ball

--- src/main/java/com/raychenon/leetcode/MoveZeroes.java
+++ src/main/java/com/raychenon/leetcode/MoveZeroes.java
@@ -10,8 +10,16 @@ public class MoveZeroes {
     /**
      * time complexity: O(n)
      * space complexity: O(1)
-     *
+     * <p>
      * solution https://leetcode.com/problems/move-zeroes/discuss/172432/THE-EASIEST-but-UNUSUAL-snowball-JAVA-solution-BEATS-100-(O(n))-%2B-clear-explanation
+     * <p>
+     * The idea is that we go through the array and gather all zeros on our road.
+     * example: input [0,1,0,3,12]
+     * [1,0,0,3,12]  Our ball size is 1. Swap the most left 0 of our snowball with element 1.
+     * [1,0,0,3,12], Our ball gets bigger, now its size = 2.
+     * [1,3,0,0,12], Swap again with the most left zero.
+     * [1,3,12,0,0], Swap again.
+     *
      * @param nums
      */
     public void moveZeroes(int[] nums) {
