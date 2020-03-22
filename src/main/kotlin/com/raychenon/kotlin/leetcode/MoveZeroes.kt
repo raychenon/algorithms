87dac3e93b59ac679dfad5077fd9baddc0c8f013
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-22 14:06:05 +0100

    #159 use snowball technique for Kotlin

--- src/main/kotlin/com/raychenon/kotlin/leetcode/MoveZeroes.kt
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/MoveZeroes.kt
@@ -10,18 +10,26 @@ object MoveZeroes {
     /**
      * time complexity: O(n)
      * space complexity: O(1)
+     *
+     * The idea is that we go through the array and gather all zeros on our road.
+     * example: input [0,1,0,3,12]
+     * [0,1,0,3,12], Our ball size is 1. First element is 0.
+     * [1,0,0,3,12], We encounter a non zero(1). Swap the most left 0 of our snowball with element 1.
+     * [1,0,0,3,12], Our ball gets bigger, now its size = 2.
+     * [1,3,0,0,12], Swap again with the most left zero.
+     * [1,3,12,0,0], Swap again.
      */
     fun moveZeroes(nums: IntArray): Unit {
 
-        var lastNonZeroFoundAt = 0
+        var zeroCount = 0
         for (i in 0..nums.size - 1) {
-            if (nums.get(i) != 0) {
-                nums[lastNonZeroFoundAt++] = nums.get(i)
+            if (nums.get(i) == 0) {
+                zeroCount += 1
+            } else if (zeroCount > 0) {
+                val temp = nums.get(i)
+                nums[i] = 0
+                nums[i - zeroCount] = temp
             }
-        }
-
-        for (i in lastNonZeroFoundAt..nums.size - 1) {
-            nums[i] = 0
         }
 
     }
--- src/test/kotlin/com/raychenon/kotlin/leetcode/MoveZeroesTest.kt
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/MoveZeroesTest.kt
@@ -18,7 +18,6 @@ class MoveZeroesTest {
         Assert.assertArrayEquals(intArrayOf(1, 3, 12, 0, 0), input)
     }
 
-
     @Test
     fun moveZeroesTest2() {
         var input = intArrayOf(0, 1, 0, 3, 5, 5, 0, 11, 12)
@@ -27,5 +26,24 @@ class MoveZeroesTest {
         Assert.assertArrayEquals(intArrayOf(1, 3, 5, 5, 11, 12, 0, 0, 0), input)
     }
 
+    @Test
+    fun allZeroesBehindTest() {
+        val input = intArrayOf(1, 2, 5, 3, 4, 0, 0, 0)
+        MoveZeroes.moveZeroes(input)
+        Assert.assertArrayEquals(intArrayOf(1, 2, 5, 3, 4, 0, 0, 0), input)
+    }
 
+    @Test
+    fun allZeroesInFrontTest() {
+        val input = intArrayOf(0, 0, 0, 1, 2, 5, 3, 4)
+        MoveZeroes.moveZeroes(input)
+        Assert.assertArrayEquals(intArrayOf(1, 2, 5, 3, 4, 0, 0, 0), input)
+    }
+
+    @Test
+    fun OneZeroesInMiddleTest() {
+        val input = intArrayOf(1, 2, 5, 0, 3, 22, 4)
+        MoveZeroes.moveZeroes(input)
+        Assert.assertArrayEquals(intArrayOf(1, 2, 5, 3, 22, 4, 0), input)
+    }
 }
