239081bb906526673d06fdf03f3bf375c7e92bd5
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-24 23:04:15 +0100

    #161 contains duplicate in python

---
+++ python3/algorithms/leetcode/contains_duplicate.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+from typing import List
+
+"""
+Given an array of integers, find if the array contains any duplicates.
+https://leetcode.com/problems/contains-duplicate/
+"""
+
+
+def containsDuplicate(nums: List[int]) -> bool:
+    collection = set()
+    for num in nums:
+        if num in collection:
+            return True
+        else:
+            collection.add(num)
+    return False
+
+
+if __name__ == "__main__":
+    assert True == containsDuplicate([1, 2, 3, 1])
+    assert False == containsDuplicate([1, 2, 3, 4])
+    assert True == containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
