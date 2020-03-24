91c17eef52f54a93831a4bf17d081dc8de88294f
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-24 23:09:10 +0100

    #161 add another way in Python

--- python3/algorithms/leetcode/contains_duplicate.py
+++ python3/algorithms/leetcode/contains_duplicate.py
@@ -16,8 +16,14 @@ def containsDuplicate(nums: List[int]) -> bool:
             collection.add(num)
     return False
 
+def containsDuplicateOneLine(nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
 
 if __name__ == "__main__":
     assert True == containsDuplicate([1, 2, 3, 1])
     assert False == containsDuplicate([1, 2, 3, 4])
     assert True == containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
+
+    assert True == containsDuplicateOneLine([1, 2, 3, 1])
+    assert False == containsDuplicateOneLine([1, 2, 3, 4])
+    assert True == containsDuplicateOneLine([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
