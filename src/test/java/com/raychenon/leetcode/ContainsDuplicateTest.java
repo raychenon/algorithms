0d2b1722818fc965c0d93ccdb3d943d4c4b453a6
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-24 20:49:43 +0100

    #161  test of containsDuplicate in Java

--- src/test/java/com/raychenon/leetcode/ContainsDuplicateTest.java
+++ src/test/java/com/raychenon/leetcode/ContainsDuplicateTest.java
@@ -1,9 +1,47 @@
 package com.raychenon.leetcode;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * User: raychenon
  * Date: 24/3/2020
- * Time: 8:37 PM
+ * https://leetcode.com/problems/contains-duplicate/
  */
 public class ContainsDuplicateTest {
+
+    private static ContainsDuplicate instance;
+
+    @BeforeClass
+    public static void setUp() {
+        instance = new ContainsDuplicate();
+    }
+
+
+    @Test
+    public void containsTest() {
+        int[] nums = {1, 2, 3, 1};
+        assertWithVariousMethods(true, nums);
+    }
+
+    @Test
+    public void noDuplicateTest() {
+        int[] nums = {1, 2, 3, 4};
+        assertWithVariousMethods(false, nums);
+    }
+
+    @Test
+    public void longArrayTest() {
+        int[] nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        assertWithVariousMethods(true, nums);
+    }
+
+    void assertWithVariousMethods(boolean expected, int[] array) {
+        assertEquals(expected, instance.containsDuplicateSort(array));
+        assertEquals(expected, instance.containsDuplicateSet(array));
+    }
+
+
 }
