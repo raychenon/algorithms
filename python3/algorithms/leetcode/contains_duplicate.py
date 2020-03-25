04255e259950a3078e26041ce5deb58d14d393a1
Raymond Chenon <raymond.chenon@gmail.com>
2020-03-25 23:00:33 +0100

    #161 comment on Python

--- python3/algorithms/leetcode/contains_duplicate.py
+++ python3/algorithms/leetcode/contains_duplicate.py
@@ -4,6 +4,10 @@ from typing import List
 """
 Given an array of integers, find if the array contains any duplicates.
 https://leetcode.com/problems/contains-duplicate/
+
+The Tortoise and Hare method won't work to find whether an array contains a duplicate => infinite loop.
+If there is at least a duplicate,  Floyd's Tortoise and Hare will find the duplicate 
+https://leetcode.com/problems/find-the-duplicate-number/
 """
 
 
@@ -16,14 +20,17 @@ def containsDuplicate(nums: List[int]) -> bool:
             collection.add(num)
     return False
 
+
 def containsDuplicateOneLine(nums: List[int]) -> bool:
-        return len(set(nums)) != len(nums)
+    return len(set(nums)) != len(nums)
+
+
+def assertDuplicateMethods(expected: bool, nums: List[int]) -> None:
+    assert expected == containsDuplicate(nums)
+    assert expected == containsDuplicateOneLine(nums)
+
 
 if __name__ == "__main__":
-    assert True == containsDuplicate([1, 2, 3, 1])
-    assert False == containsDuplicate([1, 2, 3, 4])
-    assert True == containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
-
-    assert True == containsDuplicateOneLine([1, 2, 3, 1])
-    assert False == containsDuplicateOneLine([1, 2, 3, 4])
-    assert True == containsDuplicateOneLine([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
+    assertDuplicateMethods(True, [1, 2, 3, 1])
+    assertDuplicateMethods(False, [1, 2, 3, 4])
+    assertDuplicateMethods(True, [1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
