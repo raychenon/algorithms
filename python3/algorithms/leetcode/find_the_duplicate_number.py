e6650b0f5f3bd207392314b048edb2ad3c68fd7b
Raymond Chenon <raymond.chenon@gmail.com>
2020-04-04 21:20:44 +0200

    #165 doc

--- python3/algorithms/leetcode/find_the_duplicate_number.py
+++ python3/algorithms/leetcode/find_the_duplicate_number.py
@@ -5,6 +5,9 @@ from typing import List
 def findDuplicate(nums: List[int]) -> int:
     """
     https://leetcode.com/problems/find-the-duplicate-number
+
+    time complexity: O(n)
+    space complexity: O(n)
     """
     numSet = set()
     for num in nums:
@@ -12,6 +15,12 @@ def findDuplicate(nums: List[int]) -> int:
             return num
         numSet.add(num)
 
+
+
+def assert_methods(expected_result: int, nums: List[int]):
+    assert findDuplicate(nums) == expected_result
+
+
 if __name__ == "__main__":
-    2 == findDuplicate([1,3,4,2,2])
-    3 == findDuplicate([3,1,3,4,2])
+    assert_methods(2, [1, 3, 4, 2, 2])
+    assert_methods(3, [3, 1, 3, 4, 2])
