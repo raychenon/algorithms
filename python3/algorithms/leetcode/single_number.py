b5bf1d0fce87cd47cd38899b61c36ce98f697ea3
Raymond Chenon <raymond.chenon@gmail.com>
2020-04-12 19:17:09 +0200

    #171 use python

---
+++ python3/algorithms/leetcode/single_number.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+from typing import List
+
+
+def singleNumber(nums: List[int]) -> int:
+    """
+    https://leetcode.com/problems/single-number/
+    Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+    bit manipulation with XOR operator
+    """
+    a = 0
+    for i in nums:
+        a ^= i
+    return a
+
+
+if __name__ == "__main__":
+    assert singleNumber([2, 2, 1]) == 1
+    assert singleNumber([4, 1, 2, 1, 2]) == 4
