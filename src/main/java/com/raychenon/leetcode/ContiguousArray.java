af7647eec1cc506085e73fdcda922fc1ca106eea
Raymond Chenon <raymond.chenon@gmail.com>
2020-04-15 23:32:04 +0200

    #176 contiguous array

---
+++ src/main/java/com/raychenon/leetcode/ContiguousArray.java
@@ -0,0 +1,13 @@
+package com.raychenon.leetcode;
+
+/**
+ * User: raychenon
+ * Date: 15/4/2020
+ * https://leetcode.com/problems/contiguous-array/
+ */
+public class ContiguousArray {
+
+    public int findMaxLength(int[] nums) {
+
+    }
+}
---
+++ src/test/java/com/raychenon/leetcode/ContiguousArrayTest.java
@@ -0,0 +1,32 @@
+package com.raychenon.leetcode;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * User: raychenon
+ * Date: 15/4/2020
+ * https://leetcode.com/problems/contiguous-array/
+ */
+public class ContiguousArrayTest {
+
+    private static ContiguousArray instance;
+
+    @BeforeClass
+    public static void setUp() {
+        instance = new ContiguousArray();
+    }
+
+    @Test
+    public void example1Test() {
+        assertEquals(2, instance.findMaxLength(new int[]{0, 1});
+    }
+
+    @Test
+    public void example2Test() {
+        assertEquals(2, instance.findMaxLength(new int[]{0, 1, 0});
+    }
+
+}
