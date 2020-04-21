0e298a28a2858271323d4d418a8caca656b85936
Raymond Chenon <raymond.chenon@gmail.com>
2020-04-21 23:47:27 +0200

    #179 set up tests

--- src/main/java/com/raychenon/leetcode/MinimumPathSum.java
+++ src/main/java/com/raychenon/leetcode/MinimumPathSum.java
@@ -4,6 +4,9 @@ package com.raychenon.leetcode;
  * User: raychenon
  * Date: 21/4/2020
  * https://leetcode.com/problems/minimum-path-sum/
+ *
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+ * Note: You can only move either down or right at any point in time.
  */
 public class MinimumPathSum {
 
--- src/test/java/com/raychenon/leetcode/MinimumPathSumTest.java
+++ src/test/java/com/raychenon/leetcode/MinimumPathSumTest.java
@@ -1,9 +1,32 @@
 package com.raychenon.leetcode;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * User: raychenon
  * Date: 21/4/2020
  * https://leetcode.com/problems/minimum-path-sum/
  */
 public class MinimumPathSumTest {
+
+    private static MinimumPathSum instance;
+
+    @BeforeClass
+    public static void setUp() {
+        instance = new MinimumPathSum();
+    }
+
+    @Test
+    public void example1Test() {
+        int input[][] = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        assertMinimumPathSumMethods(7, input);
+    }
+
+    private void assertMinimumPathSumMethods(int expected, int[][] input) {
+        assertEquals(expected, instance.minPathSum(input));
+    }
+
 }
