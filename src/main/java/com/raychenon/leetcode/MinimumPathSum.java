191d646d957e27af8f7e36cfac732cf409efb64d
Raymond Chenon <raymond.chenon@gmail.com>
2020-04-22 00:21:10 +0200

    #179 fix due to || instead of && condition

--- src/main/java/com/raychenon/leetcode/MinimumPathSum.java
+++ src/main/java/com/raychenon/leetcode/MinimumPathSum.java
@@ -21,18 +21,13 @@ public class MinimumPathSum {
         return minPathSumBruteForce(grid, 0, 0);
     }
 
-//    private int minPathSumBruteForce(int[][] grid, int i, int j) {
-//        if (i == grid.length || j == grid[0].length) return Integer.MAX_VALUE;
-//        // reach bottom right
-//        if (i == grid.length - 1 || j == grid[0].length - 1) return grid[i][j];
-//        return grid[i][j] + Math.min(minPathSumBruteForce(grid, i + 1, j), minPathSumBruteForce(grid, i, j + 1));
-//    }
-
     private int minPathSumBruteForce(int[][] grid, int i, int j) {
         if (i == grid.length || j == grid[0].length) return Integer.MAX_VALUE;
         // reach bottom right
         if (i == grid.length - 1 && j == grid[0].length - 1) return grid[i][j];
+
         // move from top left to bottom right
         return grid[i][j] + Math.min(minPathSumBruteForce(grid, i + 1, j), minPathSumBruteForce(grid, i, j + 1));
     }
+
 }
