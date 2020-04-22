99c4d215f72e6cbba7b15762b7badeb098da646d
Raymond Chenon <raymond.chenon@gmail.com>
2020-04-22 11:54:35 +0200

    #179 Kevin Naughton Jr solution

--- src/main/java/com/raychenon/leetcode/MinimumPathSum.java
+++ src/main/java/com/raychenon/leetcode/MinimumPathSum.java
@@ -35,22 +35,21 @@ public class MinimumPathSum {
         int nbRows = grid.length;
         int nbCols = grid[0].length;
         int[][] dp = new int[nbRows][nbCols];
-        dp[0][0] = grid[0][0];
 
-
-        for (int i = 0; i < nbRows; ++i) {
-            for (int j = 0; j < nbCols; ++j) {
-                if (i == 0 && j == 0) {
-                    dp[0][0] = grid[i][j];
-                } else {
-                    dp[i][j] = grid[i][j] + Math.min(
-                            i == 0 ? Integer.MAX_VALUE : grid[i - 1][j],
-                            j == 0 ? Integer.MAX_VALUE : grid[i][j - 1]);
+        for (int i = 0; i < nbRows; i++) {
+            for (int j = 0; j < nbCols; j++) {
+                dp[i][j] = grid[i][j];
+                if (i > 0 && j > 0) {
+                    dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
+                } else if (i > 0) {
+                    dp[i][j] += dp[i - 1][j];
+                } else if (j > 0) {
+                    dp[i][j] += dp[i][j - 1];
                 }
             }
         }
 
-        return grid[nbRows - 1][nbCols - 1];
+        return dp[nbRows - 1][nbCols - 1];
     }
 
 }
