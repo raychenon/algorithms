8c0425f97aede7a5e50d202e9ef6877f54e53626
Raymond Chenon <raymond.chenon@gmail.com>
2020-04-24 23:29:29 +0200

    #179 fix tiny errors that make whole difference

--- src/main/java/com/raychenon/leetcode/MinimumPathSum.java
+++ src/main/java/com/raychenon/leetcode/MinimumPathSum.java
@@ -111,13 +111,13 @@ public class MinimumPathSum {
         int[] dp = new int[grid[0].length];
 
         for (int i = grid.length - 1; i >= 0; i--) {
-            for (int j = grid[0].length - 1; i >= 0; j--) {
+            for (int j = grid[0].length - 1; j >= 0; j--) {
                 if (i == grid.length - 1 && j != grid[0].length - 1) {
                     dp[j] = grid[i][j] + dp[j + 1];
                 } else if (j == grid[0].length - 1 && i != grid.length - 1) {
                     dp[j] = grid[i][j] + dp[j];
                 } else if (j != grid[0].length - 1 && i != grid.length - 1) {
-                    dp[j] = grid[i][j] + dp[j] + Math.min(dp[j], dp[j + 1]);
+                    dp[j] = grid[i][j] + Math.min(dp[j], dp[j + 1]);
                 } else {
                     dp[j] = grid[i][j];
                 }
