e3dec20a33d5fa31f585f9a64444b5e4cec18a26
Raymond Chenon <raymond.chenon@gmail.com>
2020-04-26 02:03:08 +0200

    #181 shift technique

--- src/main/java/com/raychenon/leetcode/BitwiseANDofNumbersRange.java
+++ src/main/java/com/raychenon/leetcode/BitwiseANDofNumbersRange.java
@@ -4,15 +4,20 @@ package com.raychenon.leetcode;
  * User: raychenon
  * Date: 25/4/2020
  * https://leetcode.com/problems/bitwise-and-of-numbers-range/
+ * <p>
+ * Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+ * Reformulation: "given two integer numbers, we are asked to find the common prefix of their binary strings."
  */
 public class BitwiseANDofNumbersRange {
 
-    public int rangeBitwiseAndBruteForce(int m, int n) {
-        int res = m;
-        for (int i = m + 1; i <= n; i++) {
-            res = res & i;
+    public int rangeBitwiseAndShift(int m, int n) {
+        int shift = 0;
+        while (m < n) {
+            m >>= 1;
+            n >>= 1;
+            shift++;
         }
-        return res;
+        return m << shift;
     }
 
 
