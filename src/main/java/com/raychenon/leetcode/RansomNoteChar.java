7e9a2a8ac587fc650845e511125ff870c26aa04c
Raymond Chenon <raymond.chenon@gmail.com>
2020-05-03 22:32:40 +0200

    #93 correct algo

--- src/main/java/com/raychenon/leetcode/RansomNoteChar.java
+++ src/main/java/com/raychenon/leetcode/RansomNoteChar.java
@@ -14,7 +14,8 @@ public class RansomNoteChar {
     /**
      * Are all char in "ransomNote: contained in "magazine"?
      * Time complexity: O(n)
-     * Space complexity: O(M)
+     * Space complexity: O(K) ~ O(1) , where K is never more than 26
+     * (26 == number of letters in alphabet). So K is constant
      *
      * @param ransomNote
      * @param magazine
@@ -39,16 +40,26 @@ public class RansomNoteChar {
         return true;
     }
 
+    /**
+     * Time complexity: O(R*M) ~ O(n)
+     * Space complexity: O(1), the array is constant size
+     *
+     * @param ransomNote
+     * @param magazine
+     * @return
+     */
     public boolean canConstructArray(String ransomNote, String magazine) {
-        int[] chars = new int[]{26}; // 26 letters in alphabet
-        
-        for (Character c : ransomNote.toCharArray()) {
-            int index = magazine.indexOf(c, chars[c - 'a']);
-            if (index == -1)    // not found
-                return false;
-            chars[c - 'a'] = index + 1;
-        }
+        if (magazine.length() < ransomNote.length()) return false;
 
+        int ransomChars[] = new int[26];  // 26 letters in alphabet
+        for (char c : ransomNote.toCharArray()) {
+            // m.indexOf(n) run time is O(m*n). It depends on teh string length
+            int index = magazine.indexOf(c, ransomChars[c - 'a']);
+            if (index == -1) // not found
+                return false;
+            // 'a' ASCII is 97
+            ransomChars[c - 'a'] = index + 1;
+        }
         return true;
     }
 
