b5d37e0c87739bbb1737f320cd80ef8e260eb269
Raymond Chenon <raymond.chenon@gmail.com>
2020-05-09 18:07:25 +0200

    #187 TDD

---
+++ src/test/java/com/raychenon/leetcode/LongestCommonSubsequenceTest.java
@@ -0,0 +1,39 @@
+package com.raychenon.leetcode;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * User: raychenon
+ * Date: 9/5/2020
+ * https://leetcode.com/problems/longest-common-subsequence/
+ */
+public class LongestCommonSubsequenceTest {
+    private static LongestCommonSubsequence instance;
+
+    @BeforeClass
+    public static void setUp() {
+        instance = new LongestCommonSubsequence();
+    }
+
+    @Test
+    public void example1Test() {
+        assertLongestCommonSubsequenceMethods(3, "abcde", "ace");
+    }
+
+    @Test
+    public void example2Test() {
+        assertLongestCommonSubsequenceMethods(3, "abc", "abc");
+    }
+
+    @Test
+    public void example3Test() {
+        assertLongestCommonSubsequenceMethods(0, "abc", "def");
+    }
+
+    void assertLongestCommonSubsequenceMethods(int expected, String text1, String text2) {
+        assertEquals(expected, instance.longestCommonSubsequence(text1, text2));
+    }
+}
