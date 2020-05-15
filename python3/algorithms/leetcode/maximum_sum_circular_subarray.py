ba48f0ea87559782fe6f7ead68dff930a67642a0
Raymond Chenon <raymond.chenon@gmail.com>
2020-05-15 22:23:15 +0200

    #194 optimized space

--- python3/algorithms/leetcode/maximum_sum_circular_subarray.py
+++ python3/algorithms/leetcode/maximum_sum_circular_subarray.py
@@ -1,4 +1,6 @@
 from typing import List
+import sys
+
 
 def maxSubarraySumCircular(A: List[int]) -> int:
     """
@@ -8,9 +10,25 @@ def maxSubarraySumCircular(A: List[int]) -> int:
     """
 
     total_sum = 0
+    min_sum = sys.maxsize
+    max_sum = -sys.maxsize
+    acc_min = 0
+    acc_max = 0
     for e in A:
         total_sum += e
-    return total_sum
+
+        acc_max += e
+        max_sum = max(max_sum, acc_max)
+        acc_max = 0 if acc_max < 0 else acc_max
+
+        acc_min += e
+        min_sum = min(min_sum, acc_min)
+        acc_min = 0 if acc_min > 0 else acc_min
+
+    return total_sum if total_sum == min_sum else max(max_sum, total_sum - min_sum)
+
 
 if __name__ == "__main__":
-    assert maxSubarraySumCircular([1,-2,3,-2]) == 3
+    assert maxSubarraySumCircular([1, -2, 3, -2]) == 3
+    assert maxSubarraySumCircular([3, -1, 2, -1]) == 4
+    assert maxSubarraySumCircular([3, -2, 2, -3]) == 3
