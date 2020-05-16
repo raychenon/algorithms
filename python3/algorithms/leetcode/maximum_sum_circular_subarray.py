3428fb3a41f09957d25d5a39624e122d2962a319
Raymond Chenon <raymond.chenon@gmail.com>
2020-05-16 23:14:29 +0200

    #194 another method in python

--- python3/algorithms/leetcode/maximum_sum_circular_subarray.py
+++ python3/algorithms/leetcode/maximum_sum_circular_subarray.py
@@ -28,7 +28,44 @@ def maxSubarraySumCircular(A: List[int]) -> int:
     return total_sum if total_sum == min_sum else max(max_sum, total_sum - min_sum)
 
 
+def maxSubarraySumCircularSimple(A: List[int]) -> int:
+    """
+    * Time complexity: O(N)
+     * Space complexity: O(1)
+     * <p>
+     * case 1 : the max subarray  is NOT circular
+     * [    | max_subarray_sum |       ]
+     * <p>
+     * case 2 : the max subarray  is circular
+     * [max_subarray_sum1 | min_subarray_sum  | max_subarray_sum2]
+     * == total_sum - min_subarray_sum
+     * <p>
+     * Corner case
+     * If all numbers are negative, maxSum = max(A) and minSum = sum(A).
+     * <p>
+     * https://leetcode.com/problems/maximum-sum-circular-subarray/discuss/178422/One-Pass
+    :param A:
+    :return:
+    """
+    total_sum, max_sum, acc_max, min_sum, acc_min = 0, A[0], 0, A[0], 0
+    for a in A:
+        acc_max = max(acc_max + a, a)
+        max_sum = max(max_sum, acc_max)
+
+        acc_min = min(acc_min + a, a)
+        min_sum = min(min_sum, acc_min)
+
+        total_sum += a
+
+    return max(max_sum, total_sum - min_sum) if max_sum > 0 else max_sum
+
+
+def assertMaxSubarraySumCircular(A: List[int], expected: int) -> None:
+    assert maxSubarraySumCircular(A) == expected
+    assert maxSubarraySumCircularSimple(A) == expected
+
+
 if __name__ == "__main__":
-    assert maxSubarraySumCircular([1, -2, 3, -2]) == 3
-    assert maxSubarraySumCircular([3, -1, 2, -1]) == 4
-    assert maxSubarraySumCircular([3, -2, 2, -3]) == 3
+    assertMaxSubarraySumCircular([1, -2, 3, -2], 3)
+    assertMaxSubarraySumCircular([3, -1, 2, -1], 4)
+    assertMaxSubarraySumCircular([3, -2, 2, -3], 3)
