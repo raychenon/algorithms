59df37152be280723273712fecddd10598e3d9ff
Raymond Chenon <raymond.chenon@gmail.com>
2020-05-21 21:26:57 +0200

    #198 Count Square Submatrices with All Ones in Dynamic Programming

---
+++ src/main/java/com/raychenon/leetcode/CountSquareSubmatriceswithAllOnes.java
@@ -0,0 +1,25 @@
+package com.raychenon.leetcode;
+
+/**
+ * User: raychenon
+ * Date: 21/5/2020
+ * https://leetcode.com/problems/count-square-submatrices-with-all-ones
+ * <p>
+ * Given a m * n matrix of ones and zeros, return how many square submatrices (size 1*1,2*2,3*3,...,n*n) have all ones.
+ */
+public class CountSquareSubmatriceswithAllOnes {
+
+    public int countSquares(int[][] matrix) {
+        int res = 0;
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                if (matrix[r][c] > 0 && c > 0 && r > 0) {
+                    matrix[r][c] = Math.min(matrix[r][c - 1], Math.min(matrix[r - 1][c], matrix[r - 1][c - 1])) + 1;
+                }
+                res += matrix[r][c];
+            }
+        }
+
+        return res;
+    }
+}
---
+++ src/test/java/com/raychenon/leetcode/CountSquareSubmatriceswithAllOnesTest.java
@@ -0,0 +1,41 @@
+package com.raychenon.leetcode;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * User: raychenon
+ * Date: 21/5/2020
+ * https://leetcode.com/problems/count-square-submatrices-with-all-ones/
+ */
+public class CountSquareSubmatriceswithAllOnesTest {
+    private static CountSquareSubmatriceswithAllOnes countSquareSubmatriceswithAllOnes;
+
+    @BeforeClass
+    public static void setUp() {
+        countSquareSubmatriceswithAllOnes = new CountSquareSubmatriceswithAllOnes();
+    }
+
+    @Test
+    public void example1Test() {
+        int[][] matrix = {
+                {0, 1, 1, 1},
+                {1, 1, 1, 1},
+                {0, 1, 1, 1}
+        };
+        assertEquals(15, countSquareSubmatriceswithAllOnes.countSquares(matrix));
+    }
+
+    @Test
+    public void example2Test() {
+        int[][] matrix = {
+                {1, 0, 1},
+                {1, 1, 0},
+                {1, 1, 0}
+        };
+        assertEquals(7, countSquareSubmatriceswithAllOnes.countSquares(matrix));
+    }
+
+}
