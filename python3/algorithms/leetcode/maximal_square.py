c93b4701a4191660cadf67bade61e2b91538391f
Raymond Chenon <raymond.chenon@gmail.com>
2020-05-24 22:26:22 +0200

    #183 iterate

--- python3/algorithms/leetcode/maximal_square.py
+++ python3/algorithms/leetcode/maximal_square.py
@@ -14,15 +14,18 @@ def maximalSquare(matrix: List[List[str]]) -> int:
     dp = [[0] * (cols + 1) for _ in range(rows + 1)]
     max_len = 0
 
-    for r in range(1, rows):
-        for c in range(1, cols):
-            if matrix[r - 1][c - 1] == '1':
-                dp[r][c] = min(dp[r - 1][c - 1], dp[r - 1][c], dp[r][c - 1]) + 1
-                max_len = max(max_len, dp[r][c])
+    for r in range(rows):
+        for c in range(cols):
+            if matrix[r][c] == '1':
+                dp[r + 1][c + 1] = min(dp[r][c], dp[r + 1][c], dp[r][c + 1]) + 1
+                max_len = max(max_len, dp[r + 1][c + 1])
 
     print(f" max = {max_len}")
     return max_len * max_len
 
 
 if __name__ == "__main__":
-    assert 4 == maximalSquare([[1, 0, 1, 0, 0], [1, 0, 1, 1, 1], [1, 1, 1, 1, 1], [1, 0, 0, 1, 0]])
+    assert 4 == maximalSquare([[1, 0, 1, 0, 0],
+                               [1, 0, 1, 1, 1],
+                               [1, 1, 1, 1, 1],
+                               [1, 0, 0, 1, 0]])
