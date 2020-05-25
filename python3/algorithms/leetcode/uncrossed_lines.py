4e1e47ffc4143e9e7ef039c98b994fa93cbe76b3
Raymond Chenon <raymond.chenon@gmail.com>
2020-05-25 22:04:09 +0200

    #201 uncrossed lines

---
+++ python3/algorithms/leetcode/uncrossed_lines.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+def maxUncrossedLines(A: List[int], B: List[int]) -> int:
+    """"
+    https://www.youtube.com/watch?v=duCx_62nMOA
+    """
+    lenA = len(A)
+    lenB = len(B)
+
+    dp = [[0] * (lenB + 1) for _ in range(lenA + 1)]
+    for r in range(lenA):
+        for c in range(lenB):
+            if A[r] == B[c]:
+                dp[r + 1][c + 1] = 1 + dp[r][c]
+            else:
+                dp[r + 1][c + 1] = max(dp[r][c + 1], dp[r + 1][c])
+    # print(f"matrix {dp}")
+    return dp[lenA][lenB]
+
+
+if __name__ == "__main__":
+    assert maxUncrossedLines([1, 4, 2], [1, 2, 4]) == 2
+    assert maxUncrossedLines([2, 5, 1, 2, 5], [10, 5, 2, 1, 5, 2]) == 3
+    assert maxUncrossedLines([1, 3, 7, 1, 7, 5], [1, 9, 2, 5, 1]) == 2
