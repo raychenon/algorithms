563392379196650f96662e3f6c29b06cc0245298
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-01 12:30:43 +0200

    #207 Levenshtein distance

---
+++ src/test/java/com/raychenon/leetcode/EditDistanceTest.java
@@ -0,0 +1,41 @@
+package com.raychenon.leetcode;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * User: raychenon
+ * Date: 1/6/2020
+ * https://leetcode.com/problems/edit-distance
+ */
+public class EditDistanceTest {
+
+    private static EditDistance editDistance;
+
+    @BeforeClass
+    public static void setUp() {
+        editDistance = new EditDistance();
+    }
+
+    @Test
+    public void example1Test() {
+        assertEditDistance(3, "horse", "ros");
+    }
+
+    @Test
+    public void example2Test() {
+        assertEditDistance(5, "intention", "execution");
+    }
+
+    @Test
+    public void example3Test() {
+        assertEditDistance(10, "zoologicoarchaeologist", "zoogeologist");
+    }
+
+    void assertEditDistance(int expected, String word1, String word2) {
+        assertEquals(expected, editDistance.minDistance(word1, word2));
+    }
+
+}
