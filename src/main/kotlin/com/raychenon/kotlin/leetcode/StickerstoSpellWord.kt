cc41dd8b10931f5016be6ca56262f91ffadee14a
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-02 23:23:31 +0200

    #189 Stickers to Spell Word

---
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/StickerstoSpellWord.kt
@@ -0,0 +1,13 @@
+package com.raychenon.kotlin.leetcode
+
+/**
+ * User: raychenon
+ * Date: 2/6/2020
+ * https://leetcode.com/problems/stickers-to-spell-word/
+ */
+object StickerstoSpellWord {
+
+    fun minStickers(stickers: Array<String>, target: String): Int {
+
+    }
+}
