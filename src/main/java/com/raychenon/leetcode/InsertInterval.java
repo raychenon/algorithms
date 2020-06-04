356f465dcd031e3c57481910ff86c4325e2ddb0b
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-04 23:32:16 +0200

    #147 insert interval test

---
+++ src/main/java/com/raychenon/leetcode/InsertInterval.java
@@ -0,0 +1,13 @@
+package com.raychenon.leetcode;
+
+/**
+ * User: raychenon
+ * Date: 4/6/2020
+ * https://leetcode.com/problems/insert-interval/
+ */
+public class InsertInterval {
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+
+    }
+}
---
+++ src/test/java/com/raychenon/leetcode/InsertIntervalTest.java
@@ -0,0 +1,41 @@
+package com.raychenon.leetcode;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * User: raychenon
+ * Date: 4/6/2020
+ * https://leetcode.com/problems/insert-interval/
+ */
+public class InsertIntervalTest {
+
+    private static InsertInterval insertInterval;
+
+    @BeforeClass
+    public static void setUp() {
+        insertInterval = new InsertInterval();
+    }
+
+    @Test
+    public void example1Test() {
+        evaluateInsertInterval(
+                new int[][]{{1, 5}, {6, 9}},
+                new int[][]{{1, 3}, {6, 9}}, new int[]{2, 5}
+        );
+    }
+
+    @Test
+    public void example2Test() {
+        evaluateInsertInterval(
+                new int[][]{{1, 2}, {3, 10}, {12, 16}},
+                new int[][]{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, new int[]{4, 8}
+        );
+    }
+
+    void evaluateInsertInterval(int[][] expected, int[][] intervals, int[] newInterval) {
+        assertArrayEquals(expected, insertInterval.insert(intervals, newInterval));
+    }
+}
