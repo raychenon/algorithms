5bc33a33775c62bf09ce81ad9599ed73848a7457
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-05 00:17:17 +0200

    #147 more test coverage

--- src/main/kotlin/com/raychenon/kotlin/leetcode/InsertInterval.kt
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/InsertInterval.kt
@@ -37,23 +37,4 @@ object InsertInterval {
         return result.toTypedArray()
     }
 
-    fun mostCommonWord(paragraph: String, banned: Array<String>): String {
-        val re = Regex("[^A-Za-z0-9 ]")
-        val paragraphNoPunctuation = re.replace(paragraph, "")
-        val words: Array<String> = paragraphNoPunctuation.toLowerCase().split(" ").toTypedArray()
-        var mapFrequency = mutableMapOf<String, Int>()
-
-        for (word in words) {
-            mapFrequency.put(word, mapFrequency.getOrDefault(word, 0) + 1)
-        }
-
-        for (ban in banned) {
-            if (mapFrequency.containsKey(ban)) {
-                mapFrequency.remove(ban)
-            }
-        }
-
-        val max = mapFrequency.maxBy { it.value }
-        return max?.key!!
-    }
 }
