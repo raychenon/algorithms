30c2cdcba36440f0e723ba06ed1cc0cb562743fb
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-07 16:46:41 +0200

    #211 include very large test

--- src/main/java/com/raychenon/leetcode/QueueReconstructionbyHeight.java
+++ src/main/java/com/raychenon/leetcode/QueueReconstructionbyHeight.java
@@ -9,9 +9,21 @@ import java.util.List;
  * User: raychenon
  * Date: 7/6/2020
  * https://leetcode.com/problems/queue-reconstruction-by-height/
+ * <p>
+ * Suppose you have a random list of people standing in a queue.
+ * Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people
+ * in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
  */
 public class QueueReconstructionbyHeight {
 
+    /**
+     * Best explanation: https://www.youtube.com/watch?v=khddrw6Bfyw
+     * Time complexity : O(N^2) ~ O(N*log(N) + N^2)
+     * Space complexity: O(N)
+     *
+     * @param people
+     * @return
+     */
     public int[][] reconstructQueue(int[][] people) {
 
         // sort by height (index 0), then by K (index 1)
