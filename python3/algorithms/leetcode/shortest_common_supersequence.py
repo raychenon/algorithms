674181331e20f54f786d09a6bcb22eb5dbf14fe2
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-13 00:13:15 +0200

    try new Dynamic programming implementation

--- python3/algorithms/leetcode/shortest_common_supersequence.py
+++ python3/algorithms/leetcode/shortest_common_supersequence.py
@@ -1,4 +1,3 @@
-from functools import lru_cache
 """
 https://leetcode.com/problems/shortest-common-supersequence/
 
@@ -9,13 +8,20 @@ Given two strings str1 and str2, return the shortest string that has both str1 a
 
 
 def shortestCommonSupersequence(str1: str, str2: str) -> str:
-    @lru_cache(maxsize=100000)
-    def solve(i: int, j: int):
-        if i == len(str1): return str2[j:]
-        if j == len(str2): return str1[i:]
-        if str1[i] == str2[j]: return str1[i] + solve(i + 1, j + 1)
-        return min(str1[i] + solve(i + 1, j), str2[j] + solve(i, j + 1), key=len)
-    return solve(0,0)
+    len1, len2 = len(str1), len(str2)
+    dp = [['' for _ in range(len1 + 1)] for _ in range(2)]
+    for i1 in range(1, len1 + 1): dp[0][i1] = str1[:i1]  # base cases derived from str1
+    for i1 in range(1, len2 + 1):
+        dp[1][0] = str2[:i1]  # base cases derived from str2
+        for i2 in range(1, len1 + 1):
+            if str2[i1 - 1] == str1[i2 - 1]:
+                dp[1][i2] = dp[0][i2 - 1] + str1[i2 - 1]
+            else:
+                dp[1][i2] = min(dp[0][i2] + str2[i1 - 1], dp[1][i2 - 1] + str1[i2 - 1], key=len)
+        dp[0] = dp[1][:]
+        dp[1] = [""] * len(dp[1])
+    return dp[0][-1]
+
 
 if __name__ == "__main__":
     """
