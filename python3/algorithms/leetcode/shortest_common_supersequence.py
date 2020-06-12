21358465f55f7bbd5b2310b4aca1356f09638fcc
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-13 00:01:37 +0200

    doc

--- python3/algorithms/leetcode/shortest_common_supersequence.py
+++ python3/algorithms/leetcode/shortest_common_supersequence.py
@@ -1,5 +1,9 @@
 """
 https://leetcode.com/problems/shortest-common-supersequence/
+
+Given two strings str1 and str2, return the shortest string that has both str1 and str2 as subsequences.  If multiple answers exist, you may return any of them.
+
+(A string S is a subsequence of string T if deleting some number of characters from T (possibly 0, and the characters are chosen anywhere from T) results in the string S.)
 """
 
 
@@ -8,4 +12,9 @@ def shortestCommonSupersequence(str1: str, str2: str) -> str:
 
 
 if __name__ == "__main__":
+    """
+    str1 = "abac" is a subsequence of "cabac" because we can delete the first "c".
+    str2 = "cab" is a subsequence of "cabac" because we can delete the last "ac".
+    The answer provided is the shortest such string that satisfies these properties.
+    """
     assert shortestCommonSupersequence("abac", "abac") == "cabac"
