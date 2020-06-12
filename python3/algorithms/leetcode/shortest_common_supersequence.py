f46e1ce1ae84fdbcf0251ec9ef828ef32badc877
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-13 00:00:05 +0200

    #191 Shortest Common Supersequence

---
+++ python3/algorithms/leetcode/shortest_common_supersequence.py
@@ -0,0 +1,11 @@
+"""
+https://leetcode.com/problems/shortest-common-supersequence/
+"""
+
+
+def shortestCommonSupersequence(str1: str, str2: str) -> str:
+    return "cabac"
+
+
+if __name__ == "__main__":
+    assert shortestCommonSupersequence("abac", "abac") == "cabac"
