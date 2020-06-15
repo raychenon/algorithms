954d8692e6e1e243388e496a896b10cc6d2f5a89
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-15 13:13:57 +0200

    #216 write tests

--- src/main/java/com/raychenon/leetcode/CheapestFlightsWithinKStops.java
+++ src/main/java/com/raychenon/leetcode/CheapestFlightsWithinKStops.java
@@ -6,4 +6,8 @@ package com.raychenon.leetcode;
  * https://leetcode.com/problems/cheapest-flights-within-k-stops
  */
 public class CheapestFlightsWithinKStops {
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+
+    }
 }
---
+++ src/test/java/com/raychenon/leetcode/CheapestFlightsWithinKStopsTest.java
@@ -0,0 +1,37 @@
+package com.raychenon.leetcode;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * User: raychenon
+ * Date: 15/6/2020
+ * https://leetcode.com/problems/cheapest-flights-within-k-stops/
+ */
+public class CheapestFlightsWithinKStopsTest {
+
+    private static CheapestFlightsWithinKStops instance;
+
+    @BeforeClass
+    public static void setUp() {
+        instance = new CheapestFlightsWithinKStops();
+    }
+
+    @Test
+    public void flight1Test() {
+        assertMethods(200,
+                3, new int[][]{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 1);
+    }
+
+    @Test
+    public void flight2Test() {
+        assertMethods(500,
+                3, new int[][]{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 0);
+    }
+
+    private void assertMethods(int expected, int n, int[][] flights, int src, int dst, int K) {
+        assertEquals(expected, instance.findCheapestPrice(n, flights, src, dst, K));
+    }
+}
