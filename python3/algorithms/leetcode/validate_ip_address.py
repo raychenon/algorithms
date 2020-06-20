bee66a6f83d3b1e7f3f65746d7844c10a017ea5c
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-21 00:09:47 +0200

    #217 python doc

--- python3/algorithms/leetcode/validate_ip_address.py
+++ python3/algorithms/leetcode/validate_ip_address.py
@@ -1,7 +1,12 @@
 def validIPAddress(IP):
     """
     https://leetcode.com/problems/validate-ip-address/
+
+    Divide and Conquer
+    Time complexity: O(N)
+    Space complexity: O(1)
     """
+
     def isIPv4(s):
         try:
             return str(int(s)) == s and 0 <= int(s) <= 255
@@ -22,10 +27,10 @@ def validIPAddress(IP):
     return "Neither"
 
 
-
 if __name__ == "__main__":
     assert validIPAddress("172.16.254.1") == "IPv4"
     assert validIPAddress("1e1.4.5.6") == "Neither"
     assert validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334") == "IPv6"
     assert validIPAddress("02001:0db8:85a3:0000:0000:8a2e:0370:7334") == "Neither"
     assert validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334:") == "Neither"
+    assert validIPAddress("2001:0db8:85a3:0000:0:8A2E:0370:733a") == "IPv6"
