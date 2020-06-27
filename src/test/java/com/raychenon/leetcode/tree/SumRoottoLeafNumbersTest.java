dd7dacd8c6fc771fefd2aaad6a836beb7daa2b96
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-27 17:41:27 +0200

    #223 test

---
+++ src/test/java/com/raychenon/leetcode/tree/SumRoottoLeafNumbersTest.java
@@ -0,0 +1,43 @@
+package com.raychenon.leetcode.tree;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * User: raychenon
+ * Date: 27/6/2020
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/
+ */
+public class SumRoottoLeafNumbersTest {
+
+    private static SumRoottoLeafNumbers instance;
+
+    @BeforeClass
+    public static void setUp() {
+        instance = new SumRoottoLeafNumbers();
+    }
+
+    @Test
+    public void example1Test() {
+        //  [1,2,3]
+        TreeNode t1Root = new TreeNode(1);
+        t1Root.left = new TreeNode(2);
+        t1Root.right = new TreeNode(3);
+
+        assertEquals(25, instance.sumNumbersRecursive(t1Root));
+    }
+
+    @Test
+    public void example2Test() {
+        //  [4,9,0,5,1]
+        TreeNode t2Root = new TreeNode(4);
+        t2Root.left = new TreeNode(9);
+        t2Root.right = new TreeNode(0);
+        t2Root.left.left = new TreeNode(5);
+        t2Root.left.right = new TreeNode(1);
+
+        assertEquals(1026, instance.sumNumbersRecursive(t2Root));
+    }
+}
---
+++ src/test/java/com/raychenon/leetcode/tree/TreeUtil.java
@@ -0,0 +1,37 @@
+package com.raychenon.leetcode.tree;
+
+/**
+ * User: raychenon
+ * Date: 27/6/2020
+ * Time: 5:10 PM
+ */
+public class TreeUtil {
+
+    /**
+     * Input: [4,9,0,5,1]
+     * --- 4
+     * ---/ \
+     * --9   0
+     * -/ \
+     * 5   1
+     *
+     * @param values
+     * @return
+     */
+    public static TreeNode createTree(int[] values) {
+        if (values == null || values.length == 0) return null;
+
+        TreeNode root = new TreeNode(values[0]);
+        TreeNode temp = root;
+        for (int i = 1; i < values.length; i = i + 2) {
+            insertInNode(temp, values[i], values[i + 1]);
+        }
+        return root;
+    }
+
+    private static void insertInNode(TreeNode node, Integer left, Integer right) {
+        if (left != null) node.left = new TreeNode(left);
+        if (right != null) node.right = new TreeNode(right);
+    }
+
+}
