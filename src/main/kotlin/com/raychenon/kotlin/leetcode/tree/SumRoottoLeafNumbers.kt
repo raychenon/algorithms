d946ba774c14c7186f988f3c1c183488833585e4
Raymond Chenon <raymond.chenon@gmail.com>
2020-06-30 01:36:24 +0200

    #223 Sum Root to Leaf Numbers in Kotlin

---
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/tree/SumRoottoLeafNumbers.kt
@@ -0,0 +1,15 @@
+package com.raychenon.kotlin.leetcode.tree
+
+import com.raychenon.leetcode.tree.TreeNode
+
+/**
+ * User: raychenon
+ * Date: 30/6/2020
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/
+ */
+object SumRoottoLeafNumbers {
+
+    fun sumNumbers(root: TreeNode?): Int {
+
+    }
+}
---
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/tree/SumRoottoLeafNumbersTest.kt
@@ -0,0 +1,26 @@
+package com.raychenon.kotlin.leetcode.tree
+
+import com.raychenon.leetcode.tree.TreeNode
+import junit.framework.TestCase
+import org.junit.Test
+
+/**
+ * User: raychenon
+ * Date: 30/6/2020
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/
+ */
+class SumRoottoLeafNumbersTest {
+
+    @Test
+    fun example1Test() {
+        //  [1,2,3]
+        val t1Root = TreeNode(1)
+        t1Root.left = TreeNode(2)
+        t1Root.right = TreeNode(3)
+        TestCase.assertEquals(
+            25,
+            SumRoottoLeafNumbers.sumNumbers(t1Root)
+        )
+    }
+
+}
