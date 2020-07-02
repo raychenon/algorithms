4b05c5db8667e41e7f3555dd8963828b75c69f2e
Raymond Chenon <raymond.chenon@gmail.com>
2020-07-02 23:40:10 +0200

    #223 store the parent nodes

--- src/test/java/com/raychenon/leetcode/tree/TreeUtil.java
+++ src/test/java/com/raychenon/leetcode/tree/TreeUtil.java
@@ -1,5 +1,8 @@
 package com.raychenon.leetcode.tree;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * User: raychenon
  * Date: 27/6/2020
@@ -21,15 +24,16 @@ public class TreeUtil {
     public static TreeNode createTree(int[] values) {
         if (values == null || values.length == 0) return null;
 
+        List<TreeNode> nodes = new LinkedList<>(); // store the list of parent nodes
         TreeNode root = new TreeNode(values[0]);
         TreeNode temp = root;
         for (int i = 1; i < values.length; i = i + 2) {
-            insertInNode(temp, values[i], values[i + 1]);
+            insertPreOrderNode(temp, values[i], values[i + 1]);
         }
         return root;
     }
 
-    private static void insertInNode(TreeNode node, Integer left, Integer right) {
+    private static void insertPreOrderNode(TreeNode node, Integer left, Integer right) {
         if (left != null) node.left = new TreeNode(left);
         if (right != null) node.right = new TreeNode(right);
     }
