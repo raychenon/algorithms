bfcff2cca42015cc0195acbf88e7e3f81dcf65b6
Raymond Chenon <raymond.chenon@gmail.com>
2020-07-05 22:53:03 +0200

    #223 Kotlin tree creation

--- src/test/kotlin/com/raychenon/kotlin/leetcode/tree/SumRoottoLeafNumbersTest.kt
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/tree/SumRoottoLeafNumbersTest.kt
@@ -1,8 +1,8 @@
 package com.raychenon.kotlin.leetcode.tree
 
-import com.raychenon.leetcode.tree.TreeNode
 import junit.framework.TestCase
 import org.junit.Test
+import java.util.*
 
 /**
  * User: raychenon
@@ -14,13 +14,24 @@ class SumRoottoLeafNumbersTest {
     @Test
     fun example1Test() {
         //  [1,2,3]
-        val t1Root = TreeNode(1)
-        t1Root.left = TreeNode(2)
-        t1Root.right = TreeNode(3)
-        TestCase.assertEquals(
-            25,
-            SumRoottoLeafNumbers.sumNumbersRecursive(t1Root)
-        )
+        val t1Root = TreeUtil.createTree(listOf(1, 2, 3))
+
+        TestCase.assertEquals(25, SumRoottoLeafNumbers.sumNumbersRecursive(t1Root))
+    }
+
+
+    @Test
+    fun example2Test() {
+        //  [4,9,0,5,1]
+        val t2Root = TreeUtil.createTree(listOf(4, 9, 0, 5, 1))
+        TestCase.assertEquals(1026, SumRoottoLeafNumbers.sumNumbersRecursive(t2Root))
+    }
+
+    @Test
+    fun nullNodeTest() {
+        //  [4,9,0,null,1]
+        val t2Root = TreeUtil.createTree(Arrays.asList((4, 9, 0, null, 1))
+        TestCase.assertEquals(531, SumRoottoLeafNumbers.sumNumbersRecursive(t2Root))
     }
 
 }
---
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/tree/TreeUtil.kt
@@ -0,0 +1,42 @@
+package com.raychenon.kotlin.leetcode.tree
+
+import com.raychenon.leetcode.tree.TreeNode
+
+/**
+ * User: raychenon
+ * Date: 5/7/2020
+ * Time: 10:49 PM
+ */
+object TreeUtil {
+
+    /**
+     * Input: [4,9,0,5,1]
+     * --- 4
+     * ---/ \
+     * --9   0
+     * -/ \
+     * 5   1
+     *
+     * @param values
+     * @return
+     */
+    fun createTree(values: List<Int>?): TreeNode? {
+        return if (values == null || values.size == 0) null else createTree(values, 0)
+    }
+
+    private fun createTree(
+        values: List<Int>,
+        index: Int
+    ): TreeNode? {
+        if (index >= values.size) return null
+        val value = values[index] ?: return null
+        val tree = TreeNode(value)
+
+        // tree(index).left = 2 * index + 1
+        tree.left = createTree(values, index * 2 + 1)
+
+        // tree(index).right = 2 * index + 2
+        tree.right = createTree(values, index * 2 + 2)
+        return tree
+    }
+}
