b4e88a441e1920d2ae06a2d8cd52901bd727d60a
Raymond Chenon <raymond.chenon@gmail.com>
2020-07-07 00:08:32 +0200

    #223 Kotlin test

--- src/main/kotlin/com/raychenon/kotlin/leetcode/tree/SumRoottoLeafNumbers.kt
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/tree/SumRoottoLeafNumbers.kt
@@ -33,4 +33,63 @@ object SumRoottoLeafNumbers {
 
         return left + right
     }
+
+    /**
+     * Morris Preorder Traversal
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     *
+     * @param root
+     * @return
+     */
+    fun sumNumbersMorris(root: TreeNode?): Int {
+        var root = root
+        var rootToLeaf = 0
+        var currNumber = 0
+        var steps: Int
+        var predecessor: TreeNode
+        while (root != null) {
+            // If there is a left child,
+            // then compute the predecessor.
+            // If there is no link predecessor.right = root --> set it.
+            // If there is a link predecessor.right = root --> break it.
+            if (root.left != null) {
+                // Predecessor node is one step to the left
+                // and then to the right till you can.
+                predecessor = root.left
+                steps = 1
+                while (predecessor.right != null && predecessor.right !== root) {
+                    predecessor = predecessor.right
+                    ++steps
+                }
+
+                // Set link predecessor.right = root
+                // and go to explore the left subtree
+                if (predecessor.right == null) {
+                    currNumber = currNumber * 10 + root.value
+                    predecessor.right = root
+                    root = root.left
+                } else {
+                    // If you're on the leaf, update the sum
+                    if (predecessor.left == null) {
+                        rootToLeaf += currNumber
+                    }
+                    // This part of tree is explored, backtrack
+                    for (i in 0 until steps) {
+                        currNumber /= 10
+                    }
+                    predecessor.right = null
+                    root = root.right
+                }
+            } else {
+                currNumber = currNumber * 10 + root.value
+                // if you're on the leaf, update the sum
+                if (root.right == null) {
+                    rootToLeaf += currNumber
+                }
+                root = root.right
+            }
+        }
+        return rootToLeaf
+    }
 }
--- src/test/kotlin/com/raychenon/kotlin/leetcode/tree/SumRoottoLeafNumbersTest.kt
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/tree/SumRoottoLeafNumbersTest.kt
@@ -1,5 +1,6 @@
 package com.raychenon.kotlin.leetcode.tree
 
+import com.raychenon.leetcode.tree.TreeNode
 import junit.framework.TestCase
 import org.junit.Test
 
@@ -14,23 +15,34 @@ class SumRoottoLeafNumbersTest {
     fun example1Test() {
         //  [1,2,3]
         val t1Root = TreeUtil.createTree(1, 2, 3)
-
-        TestCase.assertEquals(25, SumRoottoLeafNumbers.sumNumbersRecursive(t1Root))
+        assertSumNumber(25, t1Root)
     }
-
 
     @Test
     fun example2Test() {
         //  [4,9,0,5,1]
         val t2Root = TreeUtil.createTree(4, 9, 0, 5, 1)
-        TestCase.assertEquals(1026, SumRoottoLeafNumbers.sumNumbersRecursive(t2Root))
+        assertSumNumber(1026, t2Root)
     }
 
     @Test
     fun nullNodeTest() {
         //  [4,9,0,null,1]
         val t2Root = TreeUtil.createTree(4, 9, 0, null, 1)
-        TestCase.assertEquals(531, SumRoottoLeafNumbers.sumNumbersRecursive(t2Root))
+        assertSumNumber(531, t2Root)
+    }
+
+    @Test
+    fun nullRightNodeTest() {
+        //  [4,9,0,null,1]
+        val t2Root = TreeUtil.createTree(4, 9, null, 0, 1)
+        assertSumNumber(981, t2Root)
+    }
+
+
+    private fun assertSumNumber(expected: Int, root: TreeNode?) {
+        TestCase.assertEquals(expected, SumRoottoLeafNumbers.sumNumbersRecursive(root))
+        TestCase.assertEquals(expected, SumRoottoLeafNumbers.sumNumbersMorris(root))
     }
 
 }
