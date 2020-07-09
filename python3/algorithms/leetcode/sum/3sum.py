daedbc9af5fb16054ac69daed1367ccc58893fa9
Raymond Chenon <raymond.chenon@gmail.com>
2020-07-10 00:00:24 +0200

    #229 correct

--- python3/algorithms/leetcode/sum/3sum.py
+++ python3/algorithms/leetcode/sum/3sum.py
@@ -17,15 +17,16 @@ def threeSum(nums: List[int]) -> List[List[int]]:
         seen = set()
         for j, val2 in enumerate(nums[i + 1:]):
             complement = -val1 - val2
-        if complement in seen:
-            min_val = min(val1, val2, complement)
-            max_val = max(val1, val2, complement)
-            if (min_val, max_val) not in found:
-                found.add(min_val, max_val)
-                res.append([val1, val2, complement])
+            if complement in seen:
+                min_val = min((val1, val2, complement))
+                max_val = max((val1, val2, complement))
+                if (min_val, max_val) not in found:
+                    found.add((min_val, max_val))
+                    res.append([val1, val2, complement])
             seen.add(val2)
+            
     return res
 
 
 if __name__ == '__main__':
-    assert threeSum([-1, 0, 1, 2, -1, -4]) == [[-1, 0, 1], [-1, -1, 2]]
+    assert threeSum([-1, 0, 1, 2, -1, -4]) == [[-1, 1, 0], [-1, -1, 2]]
