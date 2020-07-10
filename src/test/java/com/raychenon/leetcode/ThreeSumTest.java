9b0d635435e4f4f0b56612e7614e7208b47bc883
Raymond Chenon <raymond.chenon@gmail.com>
2020-07-10 23:29:11 +0200

    #229 increase test coverage

--- src/test/java/com/raychenon/leetcode/ThreeSumTest.java
+++ src/test/java/com/raychenon/leetcode/ThreeSumTest.java
@@ -28,6 +28,18 @@ public class ThreeSumTest {
         threeSumEval(Arrays.asList(Arrays.asList(-1, -1, 2), Arrays.asList(-1, 0, 1)), new int[]{-1, 0, 1, 2, -1, -4});
     }
 
+    @Test
+    public void example2Test() {
+        threeSumEval(Arrays.asList(Arrays.asList(-5, 2, 3)), new int[]{-1, 3, 2, 1, -5, 5});
+    }
+
+    @Test
+    public void lessThan3elementsTest() {
+        threeSumEval(Arrays.asList(), new int[]{-1, 0});
+        threeSumEval(Arrays.asList(), null);
+    }
+
+
     private void threeSumEval(List<List<Integer>> expected, int[] nums) {
         List<List<Integer>> actual = threeSum.threeSum(nums);
         assertThat(expected, is(actual));
