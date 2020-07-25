104264485ef316d1c0b9104ef11d136a0d35d658
Raymond Chenon <raymond.chenon@gmail.com>
2020-07-26 00:01:35 +0200

    #233 java implementation

---
+++ src/main/java/com/raychenon/leetcode/FindMinimuminRotatedSortedArray.java
@@ -0,0 +1,60 @@
+package com.raychenon.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * User: raychenon
+ * Date: 25/7/2020
+ * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+ */
+public class FindMinimuminRotatedSortedArray {
+
+    /**
+     * Time complexity: O(log2(n))
+     * Space complexity: O(1)
+     *
+     * @param nums
+     * @return
+     */
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+
+    /**
+     * Brute force
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     *
+     * @param nums
+     * @return
+     */
+    public int findMinBF(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num < min) min = num;
+        }
+        return min;
+    }
+
+    /**
+     * Brute force
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     *
+     * @param nums
+     * @return
+     */
+    public int findMinBFstream(int[] nums) {
+        return Arrays.stream(nums).map(v -> v).min().orElse(-1);
+    }
+}
---
+++ src/test/java/com/raychenon/leetcode/FindMinimuminRotatedSortedArrayTest.java
@@ -0,0 +1,47 @@
+package com.raychenon.leetcode;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * User: raychenon
+ * Date: 25/7/2020
+ * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+ */
+public class FindMinimuminRotatedSortedArrayTest {
+
+
+    private static FindMinimuminRotatedSortedArray instance;
+
+    @BeforeClass
+    public static void setUp() {
+        instance = new FindMinimuminRotatedSortedArray();
+    }
+
+    @Test
+    public void findMinimumTest() {
+        int[] array = {3,4,5,1,2};
+        assertMethods(1, array);
+    }
+
+    @Test
+    public void find0Test() {
+        int[] array = {4,5,6,7,0,1,2};
+        assertMethods(0, array);
+    }
+
+    @Test
+    public void uniqueElementTest() {
+        int[] array = {3};
+        assertMethods(3, array);
+    }
+
+    public void assertMethods(int expectedDuplicate, int[] array) {
+        assertEquals(expectedDuplicate, instance.findMin(array));
+        assertEquals(expectedDuplicate, instance.findMinBF(array));
+        assertEquals(expectedDuplicate, instance.findMinBFstream(array));
+    }
+
+}
