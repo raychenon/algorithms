76e7377b26a3c5dd1957d6e2cc69fe066f352dad
Raymond Chenon <raymond.chenon@gmail.com>
2020-07-28 00:04:30 +0200

    #235 find maximum in Java

--- src/test/java/com/raychenon/leetcode/FindMinimuminRotatedSortedArray2Test.java
+++ src/test/java/com/raychenon/leetcode/FindMinimuminRotatedSortedArray2Test.java
@@ -23,22 +23,29 @@ public class FindMinimuminRotatedSortedArray2Test {
     public void findMinimumTest() {
         int[] array = {4, 5, 5, 6, 6, 7, 0, 1, 2};
         assertFindMin(0, array);
+        assertFindMax(7, array);
     }
 
     @Test
     public void example1Test() {
         int[] array = {1, 3, 5};
         assertFindMin(1, array);
+        assertFindMax(5, array);
     }
 
     @Test
     public void example2Test() {
         int[] array = {2, 2, 2, 0, 1};
         assertFindMin(0, array);
+        assertFindMax(2, array);
     }
 
     void assertFindMin(int expectedDuplicate, int[] array) {
         TestCase.assertEquals(expectedDuplicate, instance.findMin(array));
+    }
+
+    void assertFindMax(int expectedDuplicate, int[] array) {
+        TestCase.assertEquals(expectedDuplicate, instance.findMax(array));
     }
 
 }
