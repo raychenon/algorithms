ec8565e3aebdc68f137d03602dcb27ec4a207ad1
Raymond Chenon <raymond.chenon@gmail.com>
2020-07-29 22:09:40 +0200

    #237 start Best Time to Buy and Sell Stock with Cooldown

---
+++ src/main/java/com/raychenon/leetcode/besttimetobuyandsell/BestTimetoBuyandSellStockWithCooldown.java
@@ -0,0 +1,13 @@
+package com.raychenon.leetcode.besttimetobuyandsell;
+
+/**
+ * User: raychenon
+ * Date: 29/7/2020
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+ */
+public class BestTimetoBuyandSellStockWithCooldown {
+
+    public int maxProfit(int[] prices) {
+
+    }
+}
