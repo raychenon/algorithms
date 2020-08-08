9bd240727b35a60585318a71a1dc0e8e1efbff47
Raymond Chenon <raymond.chenon@gmail.com>
2020-08-09 00:08:22 +0200

    #241 vertical-order-traversal-of-a-binary-tree write test

---
+++ src/main/java/com/raychenon/leetcode/tree/VerticalOrderTraversalofaBinaryTree.java
@@ -0,0 +1,16 @@
+package com.raychenon.leetcode.tree;
+
+import java.util.List;
+
+/**
+ * User: raychenon
+ * Date: 8/8/2020
+ * https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+ */
+public class VerticalOrderTraversalofaBinaryTree {
+
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+    }
+}
