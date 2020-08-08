0d989c7318e5be71cb48d88100bb6243a61dfb97
Raymond Chenon <raymond.chenon@gmail.com>
2020-08-09 00:46:40 +0200

    #241 implementation

--- src/main/java/com/raychenon/leetcode/tree/VerticalOrderTraversalofaBinaryTree.java
+++ src/main/java/com/raychenon/leetcode/tree/VerticalOrderTraversalofaBinaryTree.java
@@ -1,6 +1,9 @@
 package com.raychenon.leetcode.tree;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 /**
  * User: raychenon
@@ -11,6 +14,33 @@ public class VerticalOrderTraversalofaBinaryTree {
 
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        dfs(root, 0, 0, map);
+        List<List<Integer>> output = new ArrayList<>();
+        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+            output.add(new ArrayList<>());
+            for (PriorityQueue<Integer> nodes : ys.values()) {
+                while (!nodes.isEmpty()) {
+                    output.get(output.size() - 1).add(nodes.poll());
+                }
+            }
+        }
+        return output;
+    }
 
+    private void dfs(TreeNode root, int x, int y, TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map) {
+        if (root == null) return;
+
+        if (!map.containsKey(x)) {
+            map.put(x, new TreeMap<>());
+        }
+
+        if (!map.get(x).containsKey(y)) {
+            map.get(x).put(y, new PriorityQueue<>());
+        }
+
+        map.get(x).get(y).offer(root.value);
+        dfs(root.left, x - 1, y + 1, map);
+        dfs(root.right, x + 1, y + 1, map);
     }
 }
