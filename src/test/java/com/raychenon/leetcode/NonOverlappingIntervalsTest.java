1d38ccb874070762bd102805aa4d15199ccdda2c
Raymond Chenon <raymond.chenon@gmail.com>
2020-09-02 23:29:14 +0200

    #435 Non-overlapping Intervals

---
+++ src/test/java/com/raychenon/leetcode/NonOverlappingIntervalsTest.java
@@ -0,0 +1,44 @@
+package com.raychenon.leetcode;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * User: raychenon
+ * Date: 2/9/2020
+ * https://leetcode.com/problems/non-overlapping-intervals/
+ */
+public class NonOverlappingIntervalsTest {
+
+    private static NonOverlappingIntervals instance;
+
+    @BeforeClass
+    public static void setUp() {
+        instance = new NonOverlappingIntervals();
+    }
+
+    @Test
+    public void example1Test() {
+        int[][] input = {{1, 2}, {2, 3}, {3, 4}, {1, 3}};
+        assertNonOverlappingIntervals(1, input);
+    }
+
+    @Test
+    public void example2Test() {
+        int[][] input = {{1, 2}, {1, 2}, {1, 2}};
+        assertNonOverlappingIntervals(2, input);
+    }
+
+    @Test
+    public void example3Test() {
+        int[][] input = {{1, 2}, {2, 3}};
+        assertNonOverlappingIntervals(0, input);
+    }
+
+    private void assertNonOverlappingIntervals(int expected, int[][] arr) {
+        assertEquals(expected, instance.eraseOverlapIntervals(arr));
+    }
+
+}
