89ceaebe03b6775bb842abd5060413fb58aeb05d
Raymond Chenon <raymond.chenon@gmail.com>
2020-09-02 23:33:34 +0200

    #247 kotlin implementation

--- src/main/java/com/raychenon/leetcode/NonOverlappingIntervals.java
+++ src/main/java/com/raychenon/leetcode/NonOverlappingIntervals.java
@@ -10,14 +10,17 @@ import java.util.Arrays;
 public class NonOverlappingIntervals {
 
     /**
-     * Time complexity: O(n)
+     * Time complexity: O(n*log(n)) ~ O(n*log(n) + n) , O(n*log(n)) due to the sort
      * Space complexity: O(n)
+     *
      * @param intervals
      * @return
      */
     public int eraseOverlapIntervals(int[][] intervals) {
-        if (intervals.length < 2) {return 0;}
-        Arrays.sort(intervals, (a, b) -> (a[0]-b[0])) ;
+        if (intervals.length < 2) {
+            return 0;
+        }
+        Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
 
         int last = 0;
         int count = 0;
@@ -25,7 +28,7 @@ public class NonOverlappingIntervals {
             if (intervals[i][0] < intervals[last][1]) {
                 count++;
                 if (intervals[i][1] < intervals[last][1]) last = i;
-            }else{
+            } else {
                 last = i;
             }
         }
---
+++ src/test/kotlin/com/raychenon/kotlin/leetcode/NonOverlappingIntervalsTest.kt
@@ -0,0 +1,35 @@
+package com.raychenon.kotlin.leetcode
+
+import junit.framework.TestCase
+import org.junit.Test
+
+/**
+ * User: raychenon
+ * Date: 2/9/2020
+ * https://leetcode.com/problems/non-overlapping-intervals/
+ */
+class NonOverlappingIntervalsTest {
+
+    @Test
+    fun example1Test() {
+        val input = arrayOf(intArrayOf(1, 2), intArrayOf(2, 3), intArrayOf(3, 4), intArrayOf(1, 3))
+        assertNonOverlappingIntervals(1, input)
+    }
+
+    @Test
+    fun example2Test() {
+        val input = arrayOf(intArrayOf(1, 2), intArrayOf(1, 2), intArrayOf(1, 2))
+        assertNonOverlappingIntervals(2, input)
+    }
+
+    @Test
+    fun example3Test() {
+        val input = arrayOf(intArrayOf(1, 2), intArrayOf(2, 3))
+        assertNonOverlappingIntervals(0, input)
+    }
+
+    private fun assertNonOverlappingIntervals(expected: Int, arr: Array<IntArray>) {
+        TestCase.assertEquals(expected, NonOverlappingIntervals.eraseOverlapIntervals(arr))
+    }
+
+}
