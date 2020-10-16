bc1817ca60a03cf1080d181760c17868b31286df
Raymond Chenon <raymond.chenon@gmail.com>
2020-10-16 19:15:07 +0200

    #266 search element in 2D matrix

---
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/matrix/Search2DMatrix.kt
@@ -0,0 +1,64 @@
+package com.raychenon.kotlin.leetcode.matrix
+
+/**
+ * User: raychenon
+ * Date: 16/10/2020
+ * https://leetcode.com/problems/search-a-2d-matrix/
+ */
+object Search2DMatrix {
+
+    /**
+     * Consider the matrix (2D array) as a 1D array.
+     */
+    fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+        val row = matrix.size
+        if (row == 0) return false
+        val col = matrix[0].size
+        if (col == 0) return false
+
+        var l = 0
+        var r = col * row - 1
+        while (l != r) {
+            val mid = (l + r - 1) / 2
+            if (matrix[mid / col][mid % col] < target) {
+                l = mid + 1
+            } else {
+                r = mid
+            }
+        }
+
+        return matrix[r / col][r % col] == target
+    }
+
+
+    /**
+     * Time complexity: O(row + col)
+     * Space complexity: O(1)
+     */
+    fun searchMatrixOriginal(matrix: Array<IntArray>, target: Int): Boolean {
+        val row = matrix.size
+        if (row == 0) return false
+        val col = matrix[0].size
+        if (col == 0) return false
+
+        // find row
+        var r = 0
+        while (r < row) {
+            if (target >= matrix[r][0] && target <= matrix[r][col - 1]) {
+                break
+            } else r++
+        }
+        if (r == row) return false
+
+        // find col
+        var c = 0
+        while (c < col) {
+            if (target == matrix[r][c]) {
+                return true
+            }
+            c++
+        }
+
+        return false
+    }
+}
