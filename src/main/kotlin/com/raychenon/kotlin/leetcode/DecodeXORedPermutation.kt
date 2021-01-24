afc9b1c6413c2804c9552f5de234556e02f99d65
Raymond Chenon <raymond.chenon@gmail.com>
2021-01-24 12:15:37 +0100

    #281 Decode XORed Permutation

---
+++ src/main/kotlin/com/raychenon/kotlin/leetcode/DecodeXORedPermutation.kt
@@ -0,0 +1,41 @@
+package com.raychenon.kotlin.leetcode
+
+/**
+ * User: raychenon
+ * Date: 24/1/2021
+ * Explanations
+ * https://leetcode.com/problems/decode-xored-permutation/discuss/1031840/Explanations-XOR-and-1st-element-Java-Kotlin-Python
+ */
+object DecodeXORedPermutation {
+
+    /**
+     * encoded[i] = perm[i] XOR perm[i + 1] implies
+     * perm[i+1] = perm[i] XOR encoded[i]
+     * So we can loop to find the next perm element. But se still need to find perm[0].
+     *
+     * The array "perm" is a permutation of the first n positive integers, where n is always odd.
+     *
+     * Explanations https://leetcode.com/problems/decode-xored-permutation/discuss/1031840/Explanations-XOR-and-1st-element-Java-Kotlin-Python
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     */
+    fun decode(encoded: IntArray): IntArray {
+        var first = 0
+        val n = encoded.size + 1
+        for (i in 1..n) {
+            first = first xor i
+        }
+
+        for (i in 1 until n step 2) {
+            first = first xor encoded[i]
+        }
+
+        val perm = IntArray(n)
+        perm[0] = first
+        for (i in 0 until n - 1) {
+            perm[i + 1] = perm[i] xor encoded[i]
+        }
+
+        return perm
+    }
+}
